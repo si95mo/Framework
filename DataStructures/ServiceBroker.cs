b4bddbbb5a03872d194190b30984cc668e31f33a
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-15 12:22:13 +0200

    Add basic implementation

--- DataStructures/ServiceBroker.cs
+++ DataStructures/ServiceBroker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hardware;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,49 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    class ServiceBroker
+    public class ServiceBroker
     {
+        private static Dictionary<string, IResource> resources;
+
+        /// <summary>
+        /// The set of all the <see cref="IResource"/> subscribed
+        /// to the <see cref="ServiceBroker"/>
+        /// </summary>
+        public static Dictionary<string, IResource> Resources => resources;
+
+        /// <summary>
+        /// Initialize the <see cref="ServiceBroker"/>
+        /// </summary>
+        public static void Init()
+        {
+            resources = new Dictionary<string, IResource>();
+        }
+
+        /// <summary>
+        /// Add an <see cref="IResource"/> to 
+        /// the <see cref="ServiceBroker"/>
+        /// </summary>
+        /// <param name="resource">The <see cref="IResource"/></param>
+        public static void Add(IResource resource)
+        {
+            if (!resources.ContainsKey(resource.Code))
+                resources.Add(resource.Code, resource);
+            else
+                throw new ArgumentException("The given key was already in the collection!");
+        }
+
+        /// <summary>
+        /// Get an <see cref="IResource"/> from the collection.
+        /// </summary>
+        /// <typeparam name="IResource">The type of the item to retrieve</typeparam>
+        /// <param name="code">The <see cref="IResource"/> code to fetch</param>
+        /// <returns>The <see cref="IResource"/> if present in the collection, <see langword="null"/> otherwise</returns>
+        public static IResource Get<IResource>(string code)
+        {
+            Hardware.IResource resource = null;
+            resources.TryGetValue(code, out resource);
+
+            return (IResource)resource;
+        }
     }
 }
