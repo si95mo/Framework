d6a8737bf7aa7b6e0b3eb87431ef02ffb7c589bb
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-16 15:24:35 +0200

    Add a test project for the resource classes

---
+++ Hardware.Resources.Tests/TestClass.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardware.Resources.Tests
+{
+    public class TestClass
+    {
+        private SerialResource resource;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            resource = new SerialResource(nameof(resource), "COM99");
+            resource.Start();
+
+            resource.IsOpen.Should().BeTrue();
+            resource.Status.Should().Be(ResourceStatus.EXECUTING);
+        }
+
+        [OneTimeTearDown]
+        public void Dispose()
+        {
+            resource.Stop();
+
+            resource.IsOpen.Should().BeFalse();
+            resource.Status.Should().Be(ResourceStatus.STOPPED);
+        }
+
+        [Test]
+        [TestCase("Hello")]
+        [TestCase("Hello world!")]
+        public void SendMessage(string message)
+        {
+            resource.Send(message);
+
+            resource.Status.Should().Be(ResourceStatus.EXECUTING);
+        }
+    }
+}
