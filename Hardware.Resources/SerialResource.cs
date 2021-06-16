53408ea87fc8e7e790505e1c18b5cdeccb1e6420
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-16 15:24:16 +0200

    Add a test project for the resource classes

---
+++ Hardware.Resources/SerialResource.cs
@@ -0,0 +1,142 @@
+﻿using Core;
+using Diagnostic;
+using System;
+using System.IO.Ports;
+
+namespace Hardware.Resources
+{
+    /// <summary>
+    /// Implement a resource that communicates via the serial protocol.
+    /// See also <see cref="IResource"/>
+    /// </summary>
+    public class SerialResource : SerialPort, IResource
+    {
+        private string code;
+        private ResourceStatus status;
+        private IFailure failure;
+
+        /// <summary>
+        /// The <see cref="SerialResource"/> code
+        /// </summary>
+        public string Code => code;
+
+        /// <summary>
+        /// The <see cref="SerialResource"/> status
+        /// </summary>
+        public ResourceStatus Status => status;
+
+        /// <summary>
+        /// The <see cref="SerialResource"/> last <see cref="IFailure"/>
+        /// </summary>
+        public IFailure LastFailure => failure;
+
+        /// <summary>
+        /// Create a new instance of <see cref="SerialResource"/>
+        /// </summary>
+        public SerialResource() : base()
+        { }
+
+        /// <summary>
+        /// Create a new instance of <see cref="SerialResource"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="portName">The port name</param>
+        /// <param name="baudRate">The baud rate</param>
+        /// <param name="parity">The parity type</param>
+        /// <param name="dataBits">Data bits number</param>
+        /// <param name="stopbits">Stop bit type</param>
+        public SerialResource(string code, string portName, int baudRate = 9600, Parity parity = Parity.None,
+            int dataBits = 8, StopBits stopbits = StopBits.One) 
+            : base(portName, baudRate, parity, dataBits, stopbits)
+        {
+            this.code = code;
+            failure = new Failure();
+
+            ErrorReceived += SerialResource_ErrorReceived;
+        }
+
+        /// <summary>
+        /// Handles error in the serial communication
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The <see cref="SerialErrorReceivedEventArgs"/></param>
+        private void SerialResource_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            status = ResourceStatus.FAILURE;
+
+            SerialError error = e.EventType;
+           
+            failure.Description = error.ToString();
+            failure.Timestamp = DateTime.Now;
+
+            Logger.Log(failure.Description, SeverityType.DEBUG);
+        }
+
+        /// <summary>
+        /// Start the <see cref="SerialResource"/>
+        /// </summary>
+        public void Start()
+        {
+            failure.Clear();
+
+            status = ResourceStatus.STARTING;
+            Open();
+            status = IsOpen ? ResourceStatus.EXECUTING : ResourceStatus.FAILURE;
+
+            if (status == ResourceStatus.FAILURE)
+                failure = new Failure("Error occurred while opening the port!", DateTime.Now);
+        }
+
+        /// <summary>
+        /// Stop the <see cref="SerialResource"/>
+        /// </summary>
+        public void Stop()
+        {
+            status = ResourceStatus.STOPPING;
+            Close();
+            status = !IsOpen ? ResourceStatus.STOPPED : ResourceStatus.FAILURE;
+
+            if (status == ResourceStatus.FAILURE)
+                failure = new Failure("Error occurred while closing the port!", DateTime.Now);
+        }
+
+        public void Restart()
+        {
+            Stop();
+            Start();
+        }
+
+        /// <summary>
+        /// Send a command via serial protocol
+        /// </summary>
+        /// <param name="command">The command to send</param>
+        public void Send(string command)
+        {
+            WriteLine(command);
+        }
+
+        /// <summary>
+        /// Send a command via serial protocol and receive the response
+        /// </summary>
+        /// <param name="command">The command to send</param>
+        /// <param name="response">The response</param>
+        public void Send(string command, out string response)
+        {
+            WriteLine(command);
+            response = ReadLine();
+        }
+
+        /// <summary>
+        /// Return a description of the object
+        /// See also <see cref="object.ToString()"/>
+        /// </summary>
+        /// <returns>The description of the object</returns>
+        override
+        public string ToString()
+        {
+            string description = PortName;
+
+            return description;
+        }
+    }
+}
---
+++ Hardware.Resources/TcpResource.cs
@@ -0,0 +1,152 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardware.Resources
+{
+    /// <summary>
+    /// Implement a resource that communicates via the tcp protocol.
+    /// See also <see cref="IResource"/>
+    /// </summary>
+    class TcpResource : IResource
+    {
+        private string code;
+        private ResourceStatus status;
+        private IFailure failure;
+
+        private string ipAddress;
+        private int port;
+
+        private TcpClient tcp;
+
+        /// <summary>
+        /// The <see cref="TcpResource"/> code
+        /// </summary>
+        public string Code => code;
+
+        /// <summary>
+        /// The <see cref="TcpResource"/> status
+        /// </summary>
+        public ResourceStatus Status => status;
+
+        /// <summary>
+        /// The last <see cref="IFailure"/>
+        /// </summary>
+        public IFailure LastFailure => failure;
+
+        /// <summary>
+        /// The <see cref="TcpResource"/> operating state
+        /// </summary>
+        public bool IsOpen => tcp.Connected;
+
+        /// <summary>
+        /// The <see cref="TcpResource"/> ip address
+        /// </summary>
+        public string IpAddress => ipAddress;
+
+        public int Port => port;
+
+        /// <summary>
+        /// Create a new instance of <see cref="TcpResource"/>
+        /// </summary>
+        public TcpResource()
+        {
+            ipAddress = "";
+            port = 0;
+
+            tcp = new TcpClient();
+
+            status = ResourceStatus.STOPPED;
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="TcpResource"/>
+        /// </summary>
+        /// <param name="ipAddress">The ip address</param>
+        /// <param name="port">The port number</param>
+        public TcpResource(string ipAddress, int port)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+
+            tcp = new TcpClient();
+
+            status = ResourceStatus.STOPPED;
+        }
+
+        /// <summary>
+        /// Send a commend via tcp protocol
+        /// </summary>
+        /// <param name="request"> The http request to send </param>
+        /// <returns> The response to the request from the server </returns>
+        public void Send(string request)
+        {
+            // Request
+            var requestData = Encoding.UTF8.GetBytes(request);
+            tcp.Client.Send(requestData);
+
+            // Response
+            byte[] responseData = new byte[1024];
+            int lengthOfResponse = tcp.Client.Receive(responseData);
+        }
+
+        /// <summary>
+        /// Send a command via serial protocol and receive the response
+        /// </summary>
+        /// <param name="command">The command to send</param>
+        /// <param name="response">The response</param>
+        public void Send(string request, out string response)
+        {
+            // Request
+            var requestData = Encoding.UTF8.GetBytes(request);
+            tcp.Client.Send(requestData);
+
+            // Response
+            byte[] responseData = new byte[1024];
+            int lengthOfResponse = tcp.Client.Receive(responseData);
+
+            response = Encoding.UTF8.GetString(responseData, 0, lengthOfResponse);
+        }
+
+        /// <summary>
+        /// Restart the <see cref="TcpResource"/>
+        /// </summary>
+        public void Restart()
+        {
+            Stop();
+            Start();
+        }
+
+        /// <summary>
+        /// Start the <see cref="TcpResource"/>
+        /// </summary>
+        public void Start()
+        {
+            failure.Clear();
+
+            status = ResourceStatus.STARTING;
+            tcp.Connect(ipAddress, port);
+            status = IsOpen ? ResourceStatus.EXECUTING : ResourceStatus.FAILURE;
+
+            if (status == ResourceStatus.FAILURE)
+                failure = new Failure("Error occurred while opening the port!", DateTime.Now);
+        }
+
+        /// <summary>
+        /// Stop the <see cref="TcpResource"/>
+        /// </summary>
+        public void Stop()
+        {
+            status = ResourceStatus.STOPPING;
+            tcp.Close();
+            status = !IsOpen ? ResourceStatus.STOPPED : ResourceStatus.FAILURE;
+
+            if (status == ResourceStatus.FAILURE)
+                failure = new Failure("Error occurred while closing the port!", DateTime.Now);
+        }
+    }
+}
