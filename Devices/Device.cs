4c517dfe4edf9054915e12fcdfd758f22759f552
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-17 16:38:16 +0200

    Add a device basic implementation

---
+++ Devices/Device.cs
@@ -0,0 +1,49 @@
+﻿using Core.DataStructures;
+using Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devices
+{
+    /// <summary>
+    /// Describe a generic device.
+    /// See also <see cref="IDevice"/>
+    /// </summary>
+    class Device : IDevice
+    {
+        private string code;
+        private Bag<IChannel> channels;
+
+        /// <summary>
+        /// The <see cref="Device"/> <see cref="Bag{IProperty}"/> of 
+        /// <see cref="IChannel"/>
+        /// </summary>
+        public Bag<IChannel> Channels => channels;
+
+        /// <summary>
+        /// The <see cref="Device"/> code
+        /// </summary>
+        public string Code => code;
+
+        /// <summary>
+        /// Initialize the class attributes with 
+        /// default parameters
+        /// </summary>
+        protected Device() : this(Guid.NewGuid().ToString())
+        { }
+
+        /// <summary>
+        /// Initialize the class attributes with 
+        /// default parameters
+        /// </summary>
+        /// <param name="code">The code</param>
+        protected Device(string code)
+        {
+            this.code = code;
+            channels = new Bag<IChannel>();
+        }
+    }
+}
---
+++ Devices/IDevice.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using Core.DataStructures;
+using Hardware;
+
+namespace Devices
+{
+    /// <summary>
+    /// Describe a generic device
+    /// </summary>
+    public interface IDevice : IProperty
+    {
+        /// <summary>
+        /// The <see cref="IDevice"/> collection
+        /// of channels. See <see cref="IChannel"/>
+        /// and <see cref="Bag{IProperty}"/>
+        /// </summary>
+        Bag<IChannel> Channels
+        { get; }
+    }
+}
