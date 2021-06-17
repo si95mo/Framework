9d097afb36cf7d156b1a45a5f53d9cc6a226ada1
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-17 10:02:41 +0200

    Add a test project for data structures

---
+++ Core.DataStructures.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core.DataStructures.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Core.DataStructures.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c81ea22-4f72-4411-82a8-8cea9ff41ab3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Core.DataStructures.Tests/TestClass.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using Hardware;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataStructures.Tests
+{
+    public class TestClass
+    {
+        private Bag<IChannel<double>> bag;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            bag = new Bag<IChannel<double>>();
+
+            bag.ItemAdded += Bag_ItemAdded;
+            bag.ItemRemoved += Bag_ItemRemoved;
+
+            var input = new AnalogInput("AiFirst");
+            bag.Add(input);
+            input = new AnalogInput("AiSecond");
+            bag.Add(input);
+
+            var output = new AnalogOutput("AoFirst");
+            bag.Add(output);
+            output = new AnalogOutput("AoSecond");
+            bag.Add(output);
+
+            bag.Count.Should().Be(4);
+        }
+
+        private void Bag_ItemAdded(object sender, BagChangedEventArgs<IProperty> e)
+        {
+            bag.Count.Should().BeGreaterThan(0);
+        }
+
+        private void Bag_ItemRemoved(object sender, BagChangedEventArgs<IProperty> e)
+        {
+            bool removed = bag.Remove(e.Item.Code);
+
+            removed.Should().BeFalse();
+        }
+
+        [OneTimeTearDown]
+        public void Dispose()
+        {
+            bag.Clear();
+
+            bag.Count.Should().Be(0);
+        }
+
+        [Test]
+        [TestCase("AiFirst")]
+        [TestCase("AiSecond")]
+        [TestCase("AoFirst")]
+        [TestCase("AoSecond")]
+        public void GetItem(string code)
+        {
+            var channel = bag.Get(code);
+
+            channel.Code.Should().Be(code);
+        }
+
+        [Test]
+        [TestCase("AiFirst")]
+        [TestCase("AiSecond")]
+        [TestCase("AoFirst")]
+        [TestCase("AoSecond")]
+        public void RemoveItem(string code)
+        {
+            IProperty channel;
+
+            if (bag.Count > 0)
+                channel = bag.Get(code);
+            else
+            {
+                channel = new AnalogInput(code);
+                bag.Add(channel);
+            }
+
+            bool removed = bag.Remove(channel);
+
+            removed.Should().BeTrue();
+        }
+    }
+}
