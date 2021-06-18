f28612fc213b4caea6210764b13ff821564086ee
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-18 09:18:30 +0200

    Add parameter test project

---
+++ Core.DataStructures.Tests/ParameterTestClass.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataStructures.Tests
+{
+    class ParameterTestClass
+    {
+        private NumericParameter firstNumericParameter, secondNumericParameter;
+        private BooleanParameter firstBooleanParameter, secondBooleanParameter;
+        private StringParameter firstStringParameter, secondStringParameter;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            firstNumericParameter = new NumericParameter(nameof(firstNumericParameter));
+            secondNumericParameter = new NumericParameter(nameof(secondNumericParameter));
+
+            firstBooleanParameter = new BooleanParameter(nameof(firstBooleanParameter));
+            secondBooleanParameter = new BooleanParameter(nameof(secondBooleanParameter));
+
+            firstStringParameter = new StringParameter(nameof(firstStringParameter));
+            secondStringParameter = new StringParameter(nameof(secondStringParameter));
+
+            firstNumericParameter.ConnectTo(secondNumericParameter);
+            firstBooleanParameter.ConnectTo(secondBooleanParameter);
+            firstStringParameter.ConnectTo(secondStringParameter);
+
+            ServiceBroker.Init();
+            ServiceBroker.Add<IParameter>(firstNumericParameter);
+            ServiceBroker.Add<IParameter>(secondNumericParameter);
+            ServiceBroker.Add<IParameter>(firstBooleanParameter);
+            ServiceBroker.Add<IParameter>(secondBooleanParameter);
+            ServiceBroker.Add<IParameter>(firstStringParameter);
+            ServiceBroker.Add<IParameter>(secondStringParameter);
+        }
+
+        [OneTimeTearDown]
+        public void Dispose()
+        {
+            ServiceBroker.Clear();
+            ServiceBroker.Get<IParameter>().Count.Should().Be(0);
+        }
+
+        [Test]
+        public void ParameterTest()
+        {
+            firstNumericParameter.Value = -1.5;
+            firstNumericParameter.MeasureUnit = "V";
+
+            string toString = firstNumericParameter.ToString();
+            (toString.CompareTo("-1.5V") == 0 || toString.CompareTo("-1,5V") == 0).Should().BeTrue();
+            secondNumericParameter.Value.Should().Be(firstNumericParameter.Value);
+
+            firstBooleanParameter.Value = true;
+            secondBooleanParameter.Value.Should().Be(firstBooleanParameter.Value);
+
+            firstStringParameter.Value = "Hello world!";
+            secondStringParameter.Value.Should().Be(firstStringParameter.Value);
+        }
+    }
+}
