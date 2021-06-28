241863004630dd4c0bfc373a6a89cb90305a8a5f
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-28 16:08:34 +0200

    Add IO.File

---
+++ IO.File/JSON.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace IO.File
+{
+    /// <summary>
+    /// Handles basic IO operation for JSON files
+    /// </summary>
+    public class JSON
+    {
+        /// <summary>
+        /// Read a JSON file from disk
+        /// </summary>
+        /// <param name="path">The file to read path</param>
+        /// <returns>The <see cref="JObject"/> relative to the read file</returns>
+        public static JObject ReadJSON(string path)
+        {
+            JObject json = JObject.Parse(System.IO.File.ReadAllText(path));
+
+            return json;
+        }
+
+        /// <summary>
+        /// Save a JSON file to disk
+        /// </summary>
+        /// <param name="obj">The object to serialize and save to file</param>
+        /// <param name="path">The file path</param>
+        public static void SaveJSON(object obj, string path)
+        { 
+            System.IO.File.WriteAllText(path, JsonConvert.SerializeObject(obj));
+
+            // serialize JSON directly to a file
+            using (StreamWriter file = System.IO.File.CreateText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, obj);
+            }
+        }
+
+        /// <summary>
+        /// Save a <see cref="JObject"/> to disk.
+        /// </summary>
+        /// <param name="obj">The <see cref="JObject"/> to save</param>
+        /// <param name="path">The json file path</param>
+        public static void SaveJSON(JObject obj, string path)
+        {
+            System.IO.File.WriteAllText(path, obj.ToString());
+        }
+    }
+}
---
+++ IO.File/MAT.cs
@@ -0,0 +1,28 @@
+﻿using Accord.IO;
+using MatFileHandler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.File
+{
+    public class MAT
+    {
+        public static IStructureArray ReadStructFromFile(string path, string structName)
+        {
+            IMatFile matFile;
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                var reader = new MatFileReader(fileStream);
+                matFile = reader.Read();
+            }
+
+            IStructureArray data = matFile[structName].Value as IStructureArray;
+
+            return data;
+        }
+    }
+}
