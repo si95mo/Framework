c56b4d9829007975275912a6da5ba2215f7469c2
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-28 16:09:02 +0200

    Add Wrapper

---
+++ Wrapper/AsyncTaskWrapper.cs
@@ -0,0 +1,27 @@
+﻿using Core.DataStructures;
+using System;
+using System.Threading.Tasks;
+
+namespace Core.Scheduling.Wrapper
+{
+    public class AsyncTaskWrapper
+    {
+        /// <summary>
+        /// Wrap a <see cref="Method"/> to an awaitable <see cref="Task"/>
+        /// and start it asynchronously
+        /// </summary>
+        /// <param name="method">The <see cref="Method"/> to wrap</param>
+        /// <returns>The wrapped <see cref="Task"/></returns>
+        public static Task<string> WrapAndStart(Method method)
+        {
+            return Task.Factory.StartNew(() =>
+                {
+                    Action action = method.UnwrapAction();
+                    action?.Invoke();
+
+                    return $"Executing {method.ExtendedToString()}";
+                }
+            );
+        }
+    }
+}
---
+++ Wrapper/MethodWrapper.cs
@@ -0,0 +1,56 @@
+﻿using Core.DataStructures;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.Scheduling.Wrapper
+{
+    /// <summary>
+    /// A wrapper for <see cref="Method"/>.
+    /// </summary>
+    public class MethodWrapper
+    {
+        /// <summary>
+        /// Wrap all methods of <see cref="object"/> into
+        /// a <see cref="List{T}"/> of <see cref="Method"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="object"/> of which wrap all methods</param>
+        /// <returns>The <see cref="List{T}"/> of <see cref="Method"/></returns>
+        public static List<Method> Wrap(object obj)
+        {
+            List<Method> methodList = new List<Method>();
+
+            var methodsEnum = obj.GetType().GetMethods()
+                .Where(m => m.DeclaringType != typeof(object));
+
+            MethodInfo[] methods = new MethodInfo[methodsEnum.Count()];
+            foreach (var method in methodsEnum)
+            {
+                methodList.Add(Wrap(method, obj));
+            }
+
+            return methodList;
+        }
+
+        /// <summary>
+        /// Wrap a single <see cref="object"/> method into a <see cref="Method"/>.
+        /// </summary>
+        /// <param name="methodInfo">The <see cref="MethodInfo"/></param>
+        /// <param name="obj">The <see cref="object"/> of which wrap a <see cref="Method"/></param>
+        /// <returns>The <see cref="Method"/> wrapped</returns>
+        private static Method Wrap(MethodInfo methodInfo, object obj)
+        {
+            Method method = new Method(methodInfo.Name);
+            method.Info = methodInfo;
+            method.Object = obj;
+
+            var paramters = methodInfo.GetParameters().ToList();
+            foreach(var param in paramters)
+            {
+                method.Parameters.Add(new Parameter(param));
+            }
+
+            return method;
+        }
+    }
+}
