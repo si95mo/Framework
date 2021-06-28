c35a5133d9fb573955c9c6e09a62c00c9d34fe18
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-28 16:07:12 +0200

    Add Method and Parameter classes for scheduling

---
+++ Core.DataStructures/Method.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.DataStructures
+{
+    /// <summary>
+    /// A <see cref="ParameterList{T}"/> with OnAdd event handler.
+    /// See <see cref="List{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the list items</typeparam>
+    [Serializable]
+    public class ParameterList<T> : List<T>
+    {
+        public event EventHandler OnAdd;
+
+        public new void Add(T item)
+        {
+            if (null != OnAdd)
+                OnAdd(this, null);
+
+            base.Add(item);
+        }
+    }
+
+    /// <summary>
+    /// Class that represent a <see cref="Method"/>.
+    /// See also <see cref="Parameter"/>.
+    /// </summary>
+    [Serializable]
+    public class Method
+    {
+        private ParameterList<Parameter> parameters;
+        private int parametersCount;
+        private object obj;
+        private MethodInfo info;
+        private string name;
+        private object result;
+
+        /// <summary>
+        /// The <see cref="List{T}"/> containing the <see cref="Method"/> <see cref="Parameter"/>.
+        /// </summary>
+        public ParameterList<Parameter> Parameters => parameters;
+
+        /// <summary>
+        /// The <see cref="Method"/> <see cref="Parameter"/> count
+        /// </summary>
+        public int ParametersCount => parametersCount;
+
+        /// <summary>
+        /// The object on which to invoke the method or constructor.
+        /// See <see cref="MethodBase.Invoke(object, object[])"/>
+        /// </summary>
+        public object Object
+        {
+            get => obj;
+            set => obj = value;
+        }
+
+        /// <summary>
+        /// The <see cref="Method"/> name.
+        /// </summary>
+        public string Name => name;
+
+        /// <summary>
+        /// The <see cref="MethodInfo"/> of the <see cref="Method"/>
+        /// </summary>
+        public MethodInfo Info
+        {
+            get => info;
+            set => info = value;
+        }
+
+        /// <summary>
+        /// The result of the method.
+        /// If the method doesn't have a result,
+        /// than this property is <see langword="null"/>
+        /// </summary>
+        public object Result => result;
+
+        /// <summary>
+        /// <see langword="true"/> if the <see cref="Method"/>
+        /// has at least one <see cref="Parameter"/>.
+        /// </summary>
+        public bool HasParameters => parametersCount > 0;
+
+        /// <summary>
+        /// Initialize a new <see cref="Method"/> object with default values.
+        /// See also <see cref="Parameter"/>.
+        /// </summary>
+        public Method()
+        {
+            obj = new object();
+            result = null;
+
+            parameters = new ParameterList<Parameter>();
+            parametersCount = 0;
+
+            parameters.OnAdd += Parameters_OnAdd;
+        }
+
+        /// <summary>
+        /// Initialize a new <see cref="Method"/> object with default values
+        /// and assign it a name. 
+        /// Also see <see cref="Method.Method"/>.
+        /// </summary>
+        /// <param name="name">The <see cref="Method"/> name</param>
+        public Method(string name) : this()
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// <see cref="EventHandler"/> for parameters on add.
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The <see cref="EventArgs"/></param>
+        private void Parameters_OnAdd(object sender, EventArgs e)
+        {
+            parametersCount++;
+        }
+
+        /// <summary>
+        /// Extract the parameters' value from a <see cref="string"/> array 
+        /// containing all the parameters.
+        /// </summary>
+        /// <param name="values">The <see cref="string"/> array of parameters</param>
+        private void ExtractParameterValue(string[] values)
+        {
+            if (values != null)
+            {
+                for (int i = 0; i < Parameters.Count; i++)
+                {
+                    Parameter p = Parameters[i];
+                    p.Value = values[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Invoke a <see cref="Method"/>.
+        /// See <see cref="MethodBase.Invoke(object, object[])"/>.
+        /// </summary>
+        /// <returns>The result (<see langword="null"/> if not present)</returns>
+        public object Invoke(string[] parameters)
+        {
+            object[] p = new object[] { };
+
+            ExtractParameterValue(parameters);
+
+            if (parameters != null)
+            {
+                int n = parameters.Length;
+                p = new object[n];
+
+                for (int i = 0; i < n; i++)
+                    p[i] = this.parameters[i].Value;
+            }
+
+            result = info.Invoke(obj, p);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Perform the method action.
+        /// See also <see cref="Method.Invoke"/>
+        /// </summary>
+        /// <returns>The <see cref="Action"/> wrapped in the <see cref="Method"/></returns>
+        public Action UnwrapAction()
+        {
+            Action action = parameters.Count == 0 ?
+                new Action(() => Invoke(null)) :
+                new Action(() => Invoke(parameters.Select(x => x.ToString()).ToArray())
+            );
+
+            return action;
+        }
+
+        /// <summary>
+        /// Gives an alphabetical representation of the <see cref="Method"/>
+        /// </summary>
+        /// <returns>The description of the <see cref="Method"/></returns>
+        public override string ToString()
+        {
+            string description = Name + "(";
+
+            for (int i = 0; i < Parameters.Count; i++)
+            {
+                Parameter parameter = Parameters[i];
+                description += parameter.Value;
+
+                if (i < Parameters.Count - 1)
+                    description += ", ";
+            }
+
+            description += ")";
+
+            if (result != null)
+                description += " :: " + result.ToString();
+
+            return description;
+        }
+
+        /// <summary>
+        /// Gives an extended alphabetical representation of the <see cref="Method"/>.
+        /// See also <see cref="ToString"/>
+        /// </summary>
+        /// <returns>The extended description of the <see cref="Method"/></returns>
+        public string ExtendedToString()
+        {
+            string description = Name + "(";
+
+            for (int i = 0; i < Parameters.Count; i++)
+            {
+                Parameter parameter = Parameters[i];
+                description += parameter.Name + ": " + parameter.Value;
+
+                if (i < Parameters.Count - 1)
+                    description += ", ";
+            }
+
+            description += ")";
+
+            if (result != null)
+                description += " :: " + result.ToString();
+
+            return description;
+        }
+    }
+}
---
+++ Core.DataStructures/Parameter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Reflection;
+
+namespace Core.DataStructures
+{
+    /// <summary>
+    /// Defines the parameter type.
+    /// </summary>
+    [Serializable]
+    public enum ParameterType
+    {
+        /// <summary>
+        /// A <see cref="string"/> type parameter.
+        /// </summary>
+        String = 0,
+        /// <summary>
+        /// An <see cref="int"/> type parameter.
+        /// </summary>
+        Int = 1,
+        /// <summary>
+        /// A <see cref="double"/> type parameter.
+        /// </summary>
+        Double = 2,
+        /// <summary>
+        /// A <see cref="bool"/> type parameter.
+        /// </summary>
+        Bool = 3
+    }
+
+    /// <summary>
+    /// Defines a <see cref="Parameter"/> of a method.
+    /// See also <see cref="Method"/>.
+    /// </summary>
+    [Serializable]
+    public class Parameter
+    {
+        private object parameter;
+        private ParameterType type;
+        private string name;
+
+        /// <summary>
+        /// The value of the <see cref="Parameter"/>
+        /// </summary>
+        public object Value
+        {
+            get => parameter;
+            set => ConvertValue(value);
+        }
+
+        /// <summary>
+        /// The <see cref="Parameter"/> <see cref="ParameterType"/>
+        /// </summary>
+        public ParameterType Type => type;
+
+        /// <summary>
+        /// The name of the <see cref="Parameter"/>
+        /// </summary>
+        public string Name => name;
+
+        /// <summary>
+        /// Instantiate the <see cref="Parameter"/> object
+        /// </summary>
+        /// <param name="parameterInfo">The <see cref="ParameterInfo"/></param>
+        public Parameter(ParameterInfo parameterInfo)
+        {
+            this.parameter = new object();
+            name = parameterInfo.Name;
+
+            Type parameterType = parameterInfo.ParameterType;
+
+            if (parameterType == typeof(int))
+                type = ParameterType.Int;
+
+            if (parameterType == typeof(double))
+                type = ParameterType.Double;
+
+            if (parameterType == typeof(bool))
+                type = ParameterType.Bool;
+
+            if (parameterType == typeof(string))
+                type = ParameterType.String;
+        }
+
+        /// <summary>
+        /// Convert a general value to the actual one 
+        /// of the <see cref="Parameter"/>.
+        /// </summary>
+        /// <param name="value">The general value</param>
+        private void ConvertValue(object value)
+        {
+            switch (type)
+            {
+                case ParameterType.Int:
+                    int.TryParse(value.ToString(), out int toInt);
+                    parameter = toInt;
+                    break;
+                case ParameterType.Double:
+                    double.TryParse(value.ToString(), out double toDouble);
+                    parameter = toDouble;
+                    break;
+                case ParameterType.Bool:
+                    bool.TryParse(value.ToString(), out bool toBool);
+                    parameter = toBool;
+                    break;
+                case ParameterType.String:
+                    parameter = value.ToString();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Gives an alphabetical description of the <see cref="Parameter"/>
+        /// </summary>
+        /// <returns>A description of the object</returns>
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
