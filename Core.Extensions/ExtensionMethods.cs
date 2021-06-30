92168c9c6e77e60c8dc2402d55792f2c06c63ea9
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-06-30 17:58:50 +0200

    Add the deep clone extension methods to Core.Extensions

--- Core.Extensions/ExtensionMethods.cs
+++ Core.Extensions/ExtensionMethods.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Core.Extensions
 {
@@ -164,6 +167,37 @@ namespace Core.Extensions
                 trueAction();
             else
                 falseAction();
+        }
+    }
+
+    /// <summary>
+    /// Provides a method for performing a deep copy of an object.
+    /// Binary Serialization is used to perform the copy.
+    /// </summary>
+    // Reference Article http://www.codeproject.com/KB/tips/SerializedObjectCloner.aspx
+    public static class SystemExtension
+    {
+        /// <summary>
+        /// Perform a deep copy of the object via serialization.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>A deep copy of the object.</returns>
+        public static T Clone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable)
+                throw new ArgumentException("The type must be serializable.", nameof(source));
+
+            // Don't serialize a null object, simply return the default for that object
+            if (ReferenceEquals(source, null))
+                return default;
+
+            Stream stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, source);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return (T)formatter.Deserialize(stream);
         }
     }
 }
