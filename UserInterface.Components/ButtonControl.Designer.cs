91937eb2362d994f3093f2ded1676c7e1ef6a7f6
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-01 12:26:13 +0200

    Add a button control for UI

---
+++ UserInterface.Components/ButtonControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace UserInterface.Controls
+{
+    partial class ButtonControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnControl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnControl
+            // 
+            this.btnControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnControl.Location = new System.Drawing.Point(0, -1);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(150, 25);
+            this.btnControl.TabIndex = 0;
+            this.btnControl.Text = "Control";
+            this.btnControl.UseVisualStyleBackColor = true;
+            // 
+            // ButtonControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnControl);
+            this.Name = "ButtonControl";
+            this.Size = new System.Drawing.Size(150, 25);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnControl;
+    }
+}
---
+++ UserInterface.Components/ButtonControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterface.Controls
+{
+    public partial class ButtonControl : BaseControl
+    {
+        public override object Value
+        {
+            get => Text;
+            set => Text = (string)value;
+        }
+
+        public override string Text
+        {
+            get => btnControl.Text;
+            set => btnControl.Text = value;
+        }
+
+        public EventHandler<MouseEventArgs> ButtonClicked;
+
+        public ButtonControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
