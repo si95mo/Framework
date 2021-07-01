e64f3bea9dd2203fff815932c8de9336e9094d8b
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-01 17:10:26 +0200

    Add a TimeSpan parameter

---
+++ Core.Parameters/ITimeSpanParameter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.Parameters
+{
+    /// <summary>
+    /// Describe a generic time span parameter
+    /// </summary>
+    public interface ITimeSpanParameter : IParameter<TimeSpan>
+    {
+    }
+}
---
+++ Core.Parameters/TimeSpanParameter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Core.Parameters
+{
+    /// <summary>
+    /// Implement a time span parameter.
+    /// See also <see cref="Parameter{T}"/> and <see cref="IParameter{T}"/>
+    /// </summary>
+    public class TimeSpanParameter : Parameter<TimeSpan>, ITimeSpanParameter
+    {
+        /// <summary>
+        /// The <see cref="TimeSpanParameter"/> value
+        /// </summary>
+        public override TimeSpan Value => value;
+
+        /// <summary>
+        /// Create a new instance of <see cref="TimeSpanParameter"/>
+        /// </summary>
+        public TimeSpanParameter() : this(Guid.NewGuid().ToString())
+        { }
+
+        /// <summary>
+        /// Create a new instance of <see cref="TimeSpanParameter"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        public TimeSpanParameter(string code) : base(code)
+        { }
+
+        /// <summary>
+        /// Return a description of the object
+        /// See also <see cref="object.ToString()"/>
+        /// </summary>
+        /// <returns>The description of the object</returns>
+        public override string ToString()
+        {
+            string description = $"{value}";
+
+            return description;
+        }
+    }
+}
