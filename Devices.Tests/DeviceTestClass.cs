3b3f21910dca7b842588c48f12a7893c09b93b5a
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-01 15:56:50 +0200

    Update device test project

---
+++ Devices.Tests/DeviceTestClass.cs
@@ -0,0 +1,24 @@
+﻿using Devices.Template;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Devices.Tests
+{
+    public class DeviceTestClass
+    {
+        NewDevice device;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            device = new NewDevice("DummyDevice");
+        }
+
+        [Test]
+        public void DoTest()
+        {
+            device.Channels.Count.Should().NotBe(0);
+            device.Parameters.Count.Should().NotBe(0);
+        }
+    }
+}
