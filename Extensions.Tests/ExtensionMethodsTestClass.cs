d625505007d4465ce0806a6a4d11d0ef27eaa2bd
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-01 09:48:31 +0200

    Refactor extensions and replace Assert instruction with FluentAssertion ones

---
+++ Extensions.Tests/ExtensionMethodsTestClass.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Extensions.Tests
+{
+    public class ExtensionMethodsTestClass
+    {
+        private int item;
+        private string text;
+        bool itWorks = false;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            item = 10;
+            text = "First: {0}. Second: {1}. Third: {2}";
+        }
+        
+        [Test]
+        public void IsInTest()
+        {
+            item.IsIn(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).Should().BeFalse();
+            item.IsIn(10, 11, 12, 13).Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase("A", "B", "C")]
+        public void FormatWithTest(string first, string second, string third)
+        {
+            string tmp = text;
+            text.With(first, second, third).Should().Be(string.Format(tmp, first, second, third));
+        }
+
+        [Test]
+        public void BetweenTest()
+        {
+            item.BetweenInclusive(0, 100).Should().BeTrue();
+            item.BetweenExclusive(0, 100).Should().BeTrue();
+
+            item.BetweenInclusive(10, 100).Should().BeTrue();
+            item.BetweenExclusive(10, 100).Should().BeFalse();
+
+            item.BetweenInclusive(0, 9).Should().BeFalse();
+            item.BetweenExclusive(0, 9).Should().BeFalse();
+
+            item.BetweenInclusive(0, 10).Should().BeTrue();
+            item.BetweenExclusive(0, 10).Should().BeFalse();
+        }
+
+        [Test]
+        public void TestifTrue()
+        {
+
+            itWorks = (true.IfTrue(() => true));
+            itWorks.Should().BeTrue();
+        }
+        [Test]
+        public void TestifFalse()
+        {
+            itWorks = (false.IfFalse(() => true));
+            itWorks.Should().BeTrue();
+        }
+
+        [Test]
+        public void TestifTrueifFalse()
+        {
+            itWorks = false.IfTrueIfFalse(() => false, () => true);
+            itWorks.Should().BeTrue();
+            itWorks = false;
+            itWorks = true.IfTrueIfFalse(() => true, () => false);
+            itWorks.Should().BeTrue();
+        }
+    }
+}
