c8b4d7004214db87338bb0b0d342e4e75c7ae48a
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-02 14:35:51 +0200

    Add Instruction implementation

---
+++ Instructions/IInstruction.cs
@@ -0,0 +1,51 @@
+﻿using Core;
+using Core.DataStructures;
+using Devices;
+
+namespace Instructions
+{
+    /// <summary>
+    /// Describe a generic instruction
+    /// </summary>
+    public interface IInstruction
+    {
+        /// <summary>
+        /// The <see cref="IInstruction"/> code
+        /// </summary>
+        string Code
+        { get; }
+
+        /// <summary>
+        /// The <see cref="Bag{T}"/>
+        /// containing all the available <see cref="Method"/>
+        /// </summary>
+        Bag<Method> Methods
+        { get; }
+
+        /// <summary>
+        /// The <see cref="Bag{T}"/> of all the <see cref="IDevice"/>
+        /// with <see cref="IInstruction"/> related methods
+        /// </summary>
+        Bag<IDevice> Devices
+        { get; set; }
+
+        /// <summary>
+        /// The <see cref="IInstruction"/> <see cref="Bag{T}"/>
+        /// of input <see cref="IParameter"/>
+        /// </summary>
+        Bag<IParameter> InputParameters
+        { get; }
+
+        /// <summary>
+        /// The <see cref="IInstruction"/> <see cref="Bag{T}"/>
+        /// of output <see cref="IParameter"/>
+        /// </summary>
+        Bag<IParameter> OutputParameters
+        { get; }
+
+        /// <summary>
+        /// Execute the <see cref="IInstruction"/>
+        /// </summary>
+        void Invoke();
+    }
+}
---
+++ Instructions/Instruction.cs
@@ -0,0 +1,74 @@
+﻿using Core;
+using Core.DataStructures;
+using Core.Scheduling.Wrapper;
+using Devices;
+
+namespace Instructions
+{
+    /// <summary>
+    /// Basic implementation of a generic instruction
+    /// </summary>
+    public abstract class Instruction : IInstruction
+    {
+        protected string code;
+        protected Bag<Method> methods;
+        protected Bag<IDevice> devices;
+        private Bag<IParameter> inputParameters, outputParameters;
+
+        /// <summary>
+        /// The <see cref="Instruction"/> code
+        /// </summary>
+        public string Code => code;
+
+        /// <summary>
+        /// The <see cref="Instruction"/> <see cref="Bag{T}"/>
+        /// containing all the available <see cref="Method"/>
+        /// </summary>
+        public virtual Bag<Method> Methods => methods;
+
+        /// <summary>
+        /// The <see cref="Instruction"/> <see cref="Bag{T}"/>
+        /// of input <see cref="IParameter"/>
+        /// </summary>
+        public virtual Bag<IParameter> InputParameters => inputParameters;
+
+        /// <summary>
+        /// The <see cref="Instruction"/> <see cref="Bag{T}"/>
+        /// of output <see cref="IParameter"/>
+        /// </summary>
+        public virtual Bag<IParameter> OutputParameters => outputParameters;
+
+        /// <summary>
+        /// The <see cref="Bag{T}"/> of all the <see cref="IDevice"/>
+        /// with <see cref="Instruction"/> related methods
+        /// </summary>
+        public virtual Bag<IDevice> Devices
+        {
+            get => devices;
+            set => devices = value;
+        }
+
+        /// <summary>
+        /// Set the class attributes to default values
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="devices">The <see cref="Bag{T}"/> of devices</param>
+        protected Instruction(string code, Bag<IDevice> devices)
+        {
+            this.code = code;
+            this.devices = devices;
+
+            inputParameters = new Bag<IParameter>();
+            outputParameters = new Bag<IParameter>();
+
+            foreach (IDevice device in devices.Values)
+                foreach (Method m in MethodWrapper.Wrap(device))
+                    methods.Add(m);
+        }
+
+        /// <summary>
+        /// Execute the <see cref="Instruction"/>
+        /// </summary>
+        public abstract void Invoke();
+    }
+}
