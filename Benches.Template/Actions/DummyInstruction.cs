f0458a6b46855277d71efc9d0e5124aa3d21b6d8
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-05 15:03:29 +0200

    Add OutputParameters to ToString description

--- Benches.Template/Actions/DummyInstruction.cs
+++ Benches.Template/Actions/DummyInstruction.cs
@@ -15,6 +15,10 @@ namespace Benches.Template.Actions
     public class DummyInstruction : Instruction
     {
         private StringParameter Message = new StringParameter(nameof(Message));
+        private BooleanParameter Switch = new BooleanParameter(nameof(Switch));
+
+        private NumericParameter Result = new NumericParameter(nameof(Result));
+        private BooleanParameter NotSwitch = new BooleanParameter(nameof(NotSwitch));
 
         public DummyInstruction() : base(Guid.NewGuid().ToString(), new Bag<IDevice>())
         { }
@@ -22,11 +26,21 @@ namespace Benches.Template.Actions
         public DummyInstruction(string code, IBench bench) : base(code, bench.Devices)
         {
             InputParameters.Add(Message);
+            InputParameters.Add(Switch);
+
+            OutputParameters.Add(Result);
+            OutputParameters.Add(NotSwitch);
         }
 
         public override void Invoke()
         {
-            Console.WriteLine(Message.Value);
+            if(Switch.Value)
+                Console.WriteLine($"{Switch} :: {Message.Value}");
+            else
+                Console.WriteLine($"{Switch} :: {Message.Value}");
+
+            NotSwitch.Value = !Switch.Value;
+            Result.Value = new Random(Guid.NewGuid().GetHashCode()).Next();
         }
     }
 }
