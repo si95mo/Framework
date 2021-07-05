72ddf4817c05da23bd4bb5900570f270d3d22b8e
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-07-05 12:24:35 +0200

    Add an UI constrol for instructions

---
+++ UserInterface.Components/InstructionControl.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace UserInterface.Controls
+{
+    partial class InstructionControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblInstructionName = new System.Windows.Forms.Label();
+            this.lblMethodNamePlaceholder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.Location = new System.Drawing.Point(267, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(36, 36);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // lblInstructionName
+            // 
+            this.lblInstructionName.Location = new System.Drawing.Point(117, 24);
+            this.lblInstructionName.Name = "lblInstructionName";
+            this.lblInstructionName.Size = new System.Drawing.Size(150, 13);
+            this.lblInstructionName.TabIndex = 4;
+            this.lblInstructionName.Text = "label2";
+            // 
+            // lblMethodNamePlaceholder
+            // 
+            this.lblMethodNamePlaceholder.AutoSize = true;
+            this.lblMethodNamePlaceholder.Location = new System.Drawing.Point(2, 24);
+            this.lblMethodNamePlaceholder.Name = "lblMethodNamePlaceholder";
+            this.lblMethodNamePlaceholder.Size = new System.Drawing.Size(88, 13);
+            this.lblMethodNamePlaceholder.TabIndex = 3;
+            this.lblMethodNamePlaceholder.Text = "Instruction name:";
+            // 
+            // InstructionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblInstructionName);
+            this.Controls.Add(this.lblMethodNamePlaceholder);
+            this.Name = "InstructionControl";
+            this.Size = new System.Drawing.Size(305, 60);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblInstructionName;
+        private System.Windows.Forms.Label lblMethodNamePlaceholder;
+    }
+}
---
+++ UserInterface.Components/InstructionControl.cs
@@ -0,0 +1,140 @@
+﻿using Core;
+using Core.Parameters;
+using Core.Scheduling;
+using Instructions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterface.Controls
+{
+    public partial class InstructionControl : UserControl
+    {
+        private const int controlLocationOffset = 6;
+        private const int textBoxSize = 25;
+
+        private Size size;
+
+        private Instruction instruction;
+        private InstructionScheduler scheduler;
+        private List<BaseControl> values = new List<BaseControl>();
+
+        /// <summary>
+        /// Create a new instance of the class <see cref="InstructionControl"/>
+        /// </summary>
+        /// <param name="instruction">The <see cref="Instruction"/></param>
+        /// <param name="scheduler">The <see cref="InstructionScheduler"/></param>
+        public InstructionControl(Instruction instruction, InstructionScheduler scheduler)
+        {
+            InitializeComponent();
+
+            size = Size;
+            btnAdd.Image = Properties.Resources.ImageAdd;
+
+            this.instruction = instruction;
+            var pars = instruction.InputParameters;
+            this.scheduler = scheduler;
+
+            lblInstructionName.Text = instruction.Code;
+
+            int parameterCount = (int)pars?.Count;
+            if (parameterCount > 0)
+            {
+                size.Height += textBoxSize * parameterCount + 8;
+                Size = size;
+            }
+
+            int index = 1;
+            Label lblParam;
+            if (pars != null)
+            {
+                foreach (var p in pars.ToList())
+                {
+                    lblParam = new Label();
+                    lblParam.AutoSize = true;
+                    lblParam.Text = p.Code;
+                    lblParam.Location = new Point(
+                        lblMethodNamePlaceholder.Location.X + 10,
+                        lblMethodNamePlaceholder.Location.Y + 10
+                            + index++ * (lblMethodNamePlaceholder.Size.Height + controlLocationOffset)
+                    );
+                    lblParam.Size = lblMethodNamePlaceholder.Size;
+
+                    if (p.GetType() != typeof(BooleanParameter))
+                    {
+                        var txtCtrl = new TextControl();
+                        txtCtrl.Location = new Point(
+                            lblInstructionName.Location.X,
+                            lblParam.Location.Y - 4
+                        );
+
+                        AddElementToCollection(lblParam, txtCtrl);
+                    }
+                    else
+                    {
+                        var diCtrl = new DigitalControl();
+                        diCtrl.Location = new Point(
+                            lblInstructionName.Location.X,
+                            lblParam.Location.Y - 4
+                        );
+
+                        AddElementToCollection(lblParam, diCtrl);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add an element to the collection
+        /// </summary>
+        /// <param name="lblParam">The <see cref="Label"/></param>
+        /// <param name="control">The <see cref="BaseControl"/></param>
+        private void AddElementToCollection(Label lblParam, BaseControl control)
+        {
+            Controls.Add(lblParam);
+            Controls.Add(control);
+
+            values.Add(control);
+        }
+
+        /// <summary>
+        /// Add a <see cref="Method"/> to the <see cref="MethodScheduler"/>
+        /// <see cref="ActionQueue{T}"/>
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e"><The <see cref="EventArgs"/></param>
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            var parameters = instruction.InputParameters.ToList();
+
+            for (int i = 0; i < parameters.Count; i++)
+            {
+                var p = parameters.ElementAt(i);
+
+                switch (p.GetType())
+                {
+                    case Type boolType when boolType == typeof(BooleanParameter):
+                        (parameters.ElementAt(i) as BooleanParameter).Value = (bool)values.ElementAt(i).Value;
+                        break;
+                    case Type numType when numType == typeof(NumericParameter):
+                        (parameters.ElementAt(i) as NumericParameter).Value = (double)values.ElementAt(i).Value;
+                        break;
+                    case Type stringType when stringType == typeof(StringParameter):
+                        (parameters.ElementAt(i) as StringParameter).Value = (string)values.ElementAt(i).Value;
+                        break;
+                    case Type timeType when timeType == typeof(TimeSpanParameter):
+                        (parameters.ElementAt(i) as TimeSpanParameter).Value = (TimeSpan)values.ElementAt(i).Value;
+                        break;
+                }
+            }
+
+            scheduler.AddElement(instruction);
+        }
+    }
+}
