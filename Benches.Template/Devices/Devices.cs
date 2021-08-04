e79977c6db3bb43561ab9bb4e3a59fde94774495
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-08-04 10:10:29 +0200

    Update bench template
    Co-Authored-By: Simone <29677697+si95mo@users.noreply.github.com>

--- Benches.Template/Devices/Devices.cs
+++ Benches.Template/Devices/Devices.cs
@@ -1,5 +1,11 @@
-﻿namespace Benches.Template.Devices
+﻿using Devices.Template;
+
+namespace Benches.Template.Devices
 {
     public class Devices
-    { }
+    {
+        private NewDevice device = new NewDevice("DummyDeviceInBench");
+
+        public NewDevice Device => device;
+    }
 }
