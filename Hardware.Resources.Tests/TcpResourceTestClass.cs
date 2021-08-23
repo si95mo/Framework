dc22b082d4c036c062176c4369b99b29ba77e2f7
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-08-23 15:08:11 +0200

    Add test class for the tcp resource
    The test is performed with a tcp server simulator (like Hercules).
    The response from the server should be "Hello!" in order for the test to succeed.
    In the case of Hercules the response has to be manually inserted
    
    Co-Authored-By: Simone <29677697+si95mo@users.noreply.github.com>

---
+++ Hardware.Resources.Tests/TcpResourceTestClass.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardware.Resources.Tests
+{
+    public class TcpResourceTestClass
+    {
+        private TcpResource resource;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            resource = new TcpResource("TcpResource", GetLocalIp(), 10000);
+
+            resource.Start();
+            resource.Status.Should().Be(ResourceStatus.Executing);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            resource.Stop();
+            resource.Status.Should().Be(ResourceStatus.Stopped);
+        }
+
+        private string GetLocalIp()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var ip in host.AddressList)
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        [Test]
+        [TestCase("Hello, world!")]
+        public void Test(string message)
+        {
+            resource.Send(message + " (Send)\n");
+
+            resource.SendAndReceive(message + " (SendAndReceive)\n", out string response);
+            response.Should().Be("Hello!"); // The response is sent back from Hercules
+        }
+    }
+}
