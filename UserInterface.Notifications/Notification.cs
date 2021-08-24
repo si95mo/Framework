6154620aaf85b3b253884d5913da1ea458675669
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-08-24 15:30:25 +0200

    Add a first implementation for notification handling (just placeholders as for now)

---
+++ UserInterface.Notifications/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface.Notifications
+{
+    /// <summary>
+    /// Implement a notification mechanism in order to
+    /// inform the user of events of importance
+    /// </summary>
+    public class Notification
+    {
+    }
+}
