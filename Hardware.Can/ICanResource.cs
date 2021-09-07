65d1cddc17a221cc48de9aa3583f411cd003f7ee
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-09-07 10:00:30 +0200

    Add can resource(s)

--- Hardware.Can/ICanResource.cs
+++ Hardware.Can/ICanResource.cs
@@ -1,6 +1,44 @@
-﻿namespace Hardware.Can
+﻿using System.Collections.Generic;
+
+namespace Hardware.Can
 {
+    /// <summary>
+    /// Define a basic prototype for a can resource
+    /// </summary>
     public interface ICanResource : IResource
     {
+        /// <summary>
+        /// The collection of the filtered can id
+        /// </summary>
+        Dictionary<int, bool> FilteredCanId { get; }
+
+        /// <summary>
+        /// Send a <see cref="CanFrame"/> via the <see cref="ICanResource"/>
+        /// </summary>
+        /// <param name="canFrame">The can frame to send</param>
+        /// <returns>
+        /// <see langword="true"/> if the operation succeeded,
+        /// <see langword="false"/> otherwise
+        /// </returns>
+        bool Send(CanFrame canFrame);
+
+        /// <summary>
+        /// Enable the <see cref="ICanResource"/> log for
+        /// received <see cref="CanFrame"/>
+        /// </summary>
+        /// <param name="maxLogSize">The maximum log size</param>
+        void EnableLog(int maxLogSize = 65535);
+
+        /// <summary>
+        /// Disable the <see cref="ICanResource"/> log for
+        /// received <see cref="CanFrame"/>
+        /// </summary>
+        void DisableLog();
+
+        /// <summary>
+        /// Read the current <see cref="ICanResource"/> log
+        /// </summary>
+        /// <returns>The <see cref="string"/> containing the current log</returns>
+        string ReadLog();
     }
 }
