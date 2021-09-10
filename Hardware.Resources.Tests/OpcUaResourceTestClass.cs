da13d4634d442d307401a603dc91373d8dd948ac
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-09-10 10:10:10 +0200

    Improve test output

--- Hardware.Resources.Tests/OpcUaResourceTestClass.cs
+++ Hardware.Resources.Tests/OpcUaResourceTestClass.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Hardware.Opc.Ua;
 using NUnit.Framework;
+using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -17,9 +18,10 @@ namespace Hardware.Resources.Tests
         [OneTimeSetUp]
         public async Task Setup()
         {
-            resource = new OpcUaResource("OpcUaResource", "opc.tcp://localhost:4840/");
+            resource    = new OpcUaResource("OpcUaResource", "opc.tcp://localhost:4840/");
+
             temperature = new OpcUaInput("AiTemperature", "ns=2;s=Temperature", resource, "°C");
-            pressure = new OpcUaInput("AiPressure", "ns=2;s=Pressure", resource, "Pa");
+            pressure    = new OpcUaInput("AiPressure",    "ns=2;s=Pressure",    resource, "Pa");
 
             output = new OpcUaOutput("AoTemperature", "ns=2;s=Temperature", resource, "°C");
 
@@ -39,10 +41,16 @@ namespace Hardware.Resources.Tests
             output.Value = 10;
             Stopwatch sw = Stopwatch.StartNew();
 
+            await Task.Delay(1000);
+
             do
             {
                 temperature.Value.Should().NotBe(0.0);
                 pressure.Value.Should().NotBe(0.0);
+
+                Console.WriteLine(
+                    $"{DateTime.Now:HH:mm:ss} >> Temperature: {temperature}; pressure: {pressure}"
+                );
 
                 await Task.Delay(1000);
             } while (sw.Elapsed.TotalMilliseconds <= 10000);
