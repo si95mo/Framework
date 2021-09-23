45d8ae9bd5a4e1cc4a3b21b57d922af5a79dc238
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-09-23 17:31:52 +0200

    Add a first implementation of a multi sample analog input (to debug)

---
+++ Hardware/MultiSampleAnalogInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardware
+{
+    public class MultiSampleAnalogInput : Channel<double[]>
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="MultiSampleAnalogInput"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        public MultiSampleAnalogInput(string code, string measureUnit = "", string format = "") : base(code)
+        {
+            value = default;
+            this.measureUnit = measureUnit;
+            this.format = format;
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="MultiSampleAnalogInput"/>
+        /// </summary>
+        public MultiSampleAnalogInput() : this(Guid.NewGuid().ToString())
+        { }
+
+        /// <summary>
+        /// Give a textual description of the <see cref="Channel{T}"/>
+        /// </summary>
+        /// <returns>The textual description</returns>
+        public override string ToString()
+        {
+            double meanValue = value.ToList().Average();
+            string description = $"{meanValue.ToString(format)}{measureUnit}";
+
+            return description;
+        }
+    }
+}
