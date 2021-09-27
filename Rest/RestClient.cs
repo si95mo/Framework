8aefbc515beba91a63d16676e6e9e48d13471d9e
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-09-27 17:23:07 +0200

    Add REST functionalities

---
+++ Rest/RestClient.cs
@@ -0,0 +1,126 @@
+﻿using Core;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest
+{
+    /// <summary>
+    /// Implement a REST client
+    /// </summary>
+    public class RestClient : IProperty
+    {
+        private string code;
+        private string request;
+        private HttpResponseMessage response;
+        private string result;
+        private Uri uri;
+        private HttpClient client;
+
+        /// <summary>
+        /// The <see cref="RestClient"/> code
+        /// </summary>
+        public string Code => code;
+
+        /// <summary>
+        /// The <see cref="RestClient"/> value as <see cref="object"/>
+        /// </summary>
+        public object ValueAsObject 
+        { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
+
+        /// <summary>
+        /// The <see cref="RestClient"/> <see cref="System.Type"/>
+        /// </summary>
+        public Type Type => typeof(RestClient);
+
+        /// <summary>
+        /// The <see cref="RestClient"/> request in the form of key=value
+        /// </summary>
+        public string Request
+        {
+            get => request;
+            set => request = value;
+        }
+
+        /// <summary>
+        /// The <see cref="RestClient"/> response
+        /// </summary>
+        public HttpResponseMessage Response
+        {
+            get => response;
+            set => response = value;
+        }
+
+        /// <summary>
+        /// The <see cref="RestClient"/> query result
+        /// </summary>
+        public string Result => result;
+
+        /// <summary>
+        /// The <see cref="System.Uri"/>
+        /// </summary>
+        public Uri Uri
+        {
+            get => uri;
+            set => uri = value;
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="RestClient"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="uri">The <see cref="System.Uri"/></param>
+        /// <param name="request">The request</param>
+        public RestClient(string code, Uri uri, string request = "")
+        {
+            this.code = code;
+            this.uri = uri;
+            this.request = request;
+
+            response = new HttpResponseMessage();
+            result = "";
+
+            client = new HttpClient();
+        }
+
+        /// <summary>
+        /// Perform and async GET with the actual
+        /// <see cref="Request"/>
+        /// </summary>
+        /// <returns>The GET result</returns>
+        public async Task<string> Get()
+        {
+            string actualRequest = $"{uri}/{request}";
+            response = await client.GetAsync(actualRequest);
+            response.EnsureSuccessStatusCode();
+
+            result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Perform and async POST with the actual 
+        /// <see cref="Request"/>
+        /// </summary>
+        /// <param name="data">The data to send (as json)</param>
+        /// <returns>The POST result</returns>
+        public async Task<string> Post(object data)
+        {
+            string json = JsonConvert.SerializeObject(data);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            string actualRequest = $"{uri}/{await content.ReadAsStringAsync()}";
+            response = await client.PostAsync(uri, content);
+
+            result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+    }
+}
---
+++ Rest/RestServer.cs
@@ -0,0 +1,56 @@
+﻿using Core.Parameters;
+using Diagnostic;
+using Hardware;
+using Nancy;
+using Nancy.Hosting.Self;
+using System;
+
+namespace Rest
+{
+    /// <summary>
+    /// Implement a REST server using <see cref="Nancy"/>
+    /// </summary>
+    public class RestServer
+    {
+        private string code;
+        private Uri uri;
+        private NancyHost host;
+        private HostConfiguration configuration;
+
+        private EnumParameter<ResourceStatus> status;
+
+        /// <summary>
+        /// The <see cref="RestServer"/> code
+        /// </summary>
+        public string Code => code;
+
+        /// <summary>
+        /// The <see cref="RestServer"/> <see cref="System.Uri"/>
+        /// </summary>
+        public Uri Uri => uri;
+
+        /// <summary>
+        /// The <see cref="RestServer"/> status
+        /// </summary>
+        public EnumParameter<ResourceStatus> Status => status;
+
+        /// <summary>
+        /// Create a new instance of <see cref="RestServer"/>
+        /// </summary>
+        /// <param name="uri">The server <see cref="System.Uri"/></param>
+        public RestServer(string code, Uri uri)
+        {
+            configuration = new HostConfiguration();
+            configuration.UrlReservations.CreateAutomatically = true;
+
+            this.code = code;
+            this.uri = uri;
+
+            host = new NancyHost(uri, new DefaultNancyBootstrapper(), configuration);
+            host.Start();
+            Logger.Log($"{code} self-hosting on {uri}", Severity.Info);
+
+            status.Value = ResourceStatus.Executing;
+        }
+    }
+}
