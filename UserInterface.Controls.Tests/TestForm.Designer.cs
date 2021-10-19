7479122e169a61828490e28e820871affb89c90c
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-10-19 13:50:58 +0200

    Update test project

--- UserInterface.Controls.Tests/TestForm.Designer.cs
+++ UserInterface.Controls.Tests/TestForm.Designer.cs
@@ -29,8 +29,30 @@ namespace UserInterface.Controls.Tests
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar = new CircularProgressBar();
             this.ledControl = new UserInterface.Controls.LedControl();
             this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar.BarColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(220)))), ((int)(((byte)(4)))));
+            this.progressBar.BarColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(220)))), ((int)(((byte)(4)))));
+            this.progressBar.BarWidth = 14F;
+            this.progressBar.Font = new System.Drawing.Font("Lucida Sans Unicode", 14F);
+            this.progressBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.progressBar.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Horizontal;
+            this.progressBar.LineColor = System.Drawing.Color.DimGray;
+            this.progressBar.LineWidth = 1;
+            this.progressBar.Location = new System.Drawing.Point(230, -3);
+            this.progressBar.Maximum = ((long)(100));
+            this.progressBar.MinimumSize = new System.Drawing.Size(100, 100);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressBarShape = CircularProgressBar.ProgressShape.Flat;
+            this.progressBar.ProgressBarTextMode = CircularProgressBar.TextMode.Percentage;
+            this.progressBar.Size = new System.Drawing.Size(125, 125);
+            this.progressBar.TabIndex = 1;
+            this.progressBar.Value = ((long)(0));
             // 
             // ledControl
             // 
@@ -45,7 +67,8 @@ namespace UserInterface.Controls.Tests
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(120, 116);
+            this.ClientSize = new System.Drawing.Size(367, 134);
+            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.ledControl);
             this.Name = "TestForm";
             this.Text = "TestForm";
@@ -56,5 +79,6 @@ namespace UserInterface.Controls.Tests
         #endregion
 
         private LedControl ledControl;
+        private CircularProgressBar progressBar;
     }
 }
