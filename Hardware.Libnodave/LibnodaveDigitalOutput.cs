0b8d712a8216b9a1a9c6616f98e5963a3050ab44
Simone De Grandis <simodg95@gmail.com>
2021-11-16 09:55:28 +0100

    Add a Libnodave resource and its relative test class

---
+++ Hardware.Libnodave/LibnodaveDigitalOutput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardware.Libnodave
+{
+    /// <summary>
+    /// Implememt a digital output channel for the <see cref="LibnodaveResource"/>
+    /// </summary>
+    public class LibnodaveDigitalOutput : LibnodaveDigitalChannel
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="LibnodaveDigitalOutput"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="memoryAddress">The memory address</param>
+        /// <param name="resource">The <see cref="IResource"/></param>
+        public LibnodaveDigitalOutput(string code, int memoryAddress, IResource resource) : base(code, memoryAddress, resource)
+        {
+            ValueChanged += LibnodaveDigitalOutput_ValueChanged;
+        }
+
+        private async void LibnodaveDigitalOutput_ValueChanged(object sender, Core.ValueChangedEventArgs e)
+        {
+            await (resource as LibnodaveResource).Send(code);
+        }
+    }
+}
---
+++ Hardware.Libnodave/LibnodaveResource.cs
@@ -0,0 +1,255 @@
+﻿using Core;
+using Diagnostic;
+using System;
+using System.Threading.Tasks;
+using static libnodave;
+
+namespace Hardware.Libnodave
+{
+    /// <summary>
+    /// Implement a <see cref="Resource"/> that communicates with PLCs using LibNoDave
+    /// </summary>
+    public class LibnodaveResource : Resource
+    {
+        private string ipAddress;
+        private int port;
+
+        private daveOSserialType connectionType; // The connection type
+        private daveInterface connectionInterface; // The connection interface
+        private daveConnection connection; // The connection
+
+        private byte[] buffer;
+        private int bufferSize;
+        private int rack;
+        private int slot;
+
+        private bool isOpen;
+
+        public override bool IsOpen => isOpen;
+
+        /// <summary>
+        /// Create a new instance of <see cref="LibnodaveResource"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="ipAddress">The ip address</param>
+        /// <param name="port">The port</param>
+        /// <param name="bufferSize">The buffer size (memory bytes to read)</param>
+        /// <param name="rack">The rack number</param>
+        /// <param name="slot">The slot number</param>
+        public LibnodaveResource(string code, string ipAddress, int port, int bufferSize, int rack, int slot) : base(code)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+
+            this.bufferSize = bufferSize;
+            buffer = new byte[bufferSize];
+
+            this.rack = rack;
+            this.slot = slot;
+
+            isOpen = false;
+        }
+
+        public override async Task Restart()
+        {
+            Stop();
+            await Start();
+        }
+
+        public override async Task Start()
+        {
+            int res = 0;
+            await Task.Run(() =>
+                {
+                    Status.Value = ResourceStatus.Starting;
+
+                    connectionType.rfd = openSocket(port, ipAddress);
+                    connectionType.wfd = connectionType.rfd;
+                    connectionInterface = new daveInterface(connectionType, code, 0, daveProtoISOTCP, daveSpeed187k);
+
+                    res = connectionInterface.initAdapter();
+                    connection = new daveConnection(connectionInterface, 0, rack, slot);
+                    res = connection.connectPLC();
+
+                    if (res == 0)
+                    {
+                        isOpen = res == 0;
+                        Status.Value = ResourceStatus.Executing;
+                    }
+                    else
+                    {
+                        Status.Value = ResourceStatus.Failure;
+                        Logger.Log($"Unable to connect to the PLC at {ipAddress}! (Error code: {res})", Severity.Error);
+                        failure = new Failure($"Unable to connect to the PLC at {ipAddress}! (Error code: {res})");
+                    }
+                }
+            );
+        }
+
+        public override void Stop()
+        {
+            Status.Value = ResourceStatus.Stopping;
+
+            int res = connection.disconnectPLC();
+            isOpen = res == 0;
+
+            Status.Value = !isOpen ? ResourceStatus.Stopped : ResourceStatus.Failure;
+        }
+
+        /// <summary>
+        /// Send a value through the <see cref="LibnodaveResource"/>
+        /// </summary>
+        /// <param name="code">The channel code with the value to send</param>
+        /// <returns>The async <see cref="Task"/></returns>
+        internal async Task Send(string code)
+        {
+            ILibnodaveChannel channel = channels.Get(code) as ILibnodaveChannel;
+
+            if (Status.Value == ResourceStatus.Executing)
+            {
+                await Task.Run(() =>
+                    {
+                        if (channel is LibnodaveDigitalOutput)
+                            buffer[channel.MemoryAddress] = BitConverter.GetBytes((channel as LibnodaveDigitalOutput).Value)[0];
+                        else
+                        {
+                            LibnodaveAnalogChannel analogChannel = channel as LibnodaveAnalogChannel;
+
+                            int n = ExtractNumberOfBytes(channel);
+                            byte[] array = new byte[n];
+
+                            switch (analogChannel.Representation)
+                            {
+                                case NumericRepresentation.UInt16:
+                                    array = BitConverter.GetBytes((ushort)analogChannel.Value);
+                                    break;
+
+                                case NumericRepresentation.Int32:
+                                    array = BitConverter.GetBytes((int)analogChannel.Value);
+                                    break;
+
+                                case NumericRepresentation.Single:
+                                    array = BitConverter.GetBytes((float)analogChannel.Value);
+                                    break;
+
+                                case NumericRepresentation.Double:
+                                    array = BitConverter.GetBytes(analogChannel.Value);
+                                    break;
+                            }
+
+                            Array.Copy(array, 0, buffer, channel.MemoryAddress, n);
+                        }
+
+                        int res = connection.writeBytes(daveInputs, 0, 0, bufferSize, buffer);
+                        if (res != 0)
+                        {
+                            Logger.Log($"Unable the perform the write operation! (Error code: {res})", Severity.Error);
+                            failure = new Failure($"Unable to perform the write operation! (Error code: {res})");
+                        }
+                    }
+                );
+            }
+        }
+
+        /// <summary>
+        /// Receive a value through the <see cref="LibnodaveResource"/>
+        /// </summary>
+        /// <param name="code">The channel code in which the value will be stored</param>
+        /// <returns>The async <see cref="Task"/></returns>
+        internal async Task Receive(string code)
+        {
+            ILibnodaveChannel channel = channels.Get(code) as ILibnodaveChannel;
+
+            if (Status.Value == ResourceStatus.Executing)
+            {
+                await Task.Run(() =>
+                    {
+                        int res = connection.readBytes(daveInputs, 0, 0, bufferSize, buffer); // Read the memory buffer
+
+                    if (res == 0) // Read ok
+                    {
+                            if (channel is LibnodaveDigitalInput)
+                                (channel as LibnodaveDigitalInput).Value = Convert.ToBoolean(buffer[channel.MemoryAddress]);
+                            else
+                            {
+                            // Memory buffer manipulation - get only the elements of interest
+                            int n = ExtractNumberOfBytes(channel);
+                                byte[] array = new byte[n];
+                                Array.Copy(buffer, channel.MemoryAddress, array, 0, n);
+
+                            // Channel value assignment
+                            switch (channel.RepresentationBytes)
+                                {
+                                    case RepresentationBytes.Eight:
+                                        (channel as LibnodaveAnalogInput).Value = BitConverter.ToDouble(array, 0);
+                                        break;
+
+                                    case RepresentationBytes.Four:
+                                        switch ((channel as LibnodaveAnalogChannel).Representation)
+                                        {
+                                            case NumericRepresentation.Single:
+                                                (channel as LibnodaveAnalogInput).Value = BitConverter.ToSingle(array, 0);
+                                                break;
+
+                                            case NumericRepresentation.Int32:
+                                                (channel as LibnodaveAnalogInput).Value = BitConverter.ToUInt32(array, 0);
+                                                break;
+                                        }
+                                        break;
+
+                                    case RepresentationBytes.Two:
+                                        (channel as LibnodaveAnalogInput).Value = BitConverter.ToUInt16(array, 0);
+                                        break;
+
+                                    case RepresentationBytes.One:
+                                        (channel as LibnodaveAnalogInput).Value = array[0];
+                                        break;
+                                }
+                            }
+                        }
+                        else // Error during read
+                    {
+                            Logger.Log("Unable to perform the read operation!", Severity.Error);
+                            failure = new Failure("Unable to perform the read operation!");
+                        }
+                    }
+                );
+            }
+        }
+
+        /// <summary>
+        /// Extract the number of bytes based on <see cref="RepresentationBytes"/>
+        /// </summary>
+        /// <param name="channel">The <see cref="ILibnodaveChannel"/></param>
+        /// <returns>The number of bytes</returns>
+        private int ExtractNumberOfBytes(ILibnodaveChannel channel)
+        {
+            int numberOfBytes;
+
+            switch (channel.RepresentationBytes)
+            {
+                case RepresentationBytes.One: // byte
+                    numberOfBytes = 1;
+                    break;
+
+                case RepresentationBytes.Two: // ushort
+                    numberOfBytes = 2;
+                    break;
+
+                case RepresentationBytes.Four: // int and float
+                    numberOfBytes = 4;
+                    break;
+
+                case RepresentationBytes.Eight: // double
+                    numberOfBytes = 8;
+                    break;
+
+                default:
+                    numberOfBytes = 4;
+                    break;
+            }
+
+            return numberOfBytes;
+        }
+    }
+}
