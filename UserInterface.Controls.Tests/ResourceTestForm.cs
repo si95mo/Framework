4b9c2f8f14fe638e4fe17ae8ceaf5ce41fc5a17b
Simone De Grandis <simodg95@gmail.com>
2021-11-18 09:46:59 +0100

    Update UI test project (for ResourceControl)

---
+++ UserInterface.Controls.Tests/ResourceTestForm.cs
@@ -0,0 +1,30 @@
+﻿using Hardware.Modbus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterface.Controls.Tests
+{
+    public partial class ResourceTestForm : CustomForm
+    {
+        public ResourceTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void ResourceTestForm_Load(object sender, EventArgs e)
+        {
+            ModbusResource resource = new ModbusResource("ModbusResource", "123.456.789.000");
+            await resource.Start();
+
+            ResourceControl resourceControl = new ResourceControl(resource, layoutPanel);
+            layoutPanel.Controls.Add(resourceControl);
+        }
+    }
+}
