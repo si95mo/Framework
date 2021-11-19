38d8f6c529eef35470cb10348666c5a439c26673
Simone De Grandis <simodg95@gmail.com>
2021-11-19 09:59:03 +0100

    Update NeuralNetwork class and relative test project

--- ArtificialIntelligence/NeuralNetwork.cs
+++ ArtificialIntelligence/NeuralNetwork.cs
@@ -1,250 +1,199 @@
-﻿using System;
+﻿using Encog.Engine.Network.Activation;
+using Encog.ML.Data;
+using Encog.ML.Data.Basic;
+using Encog.ML.Train;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Neural.Networks.Training.Propagation.Resilient;
 using System.Collections.Generic;
-using System.IO;
 
 namespace ArtificialIntelligence
 {
+    /// <summary>
+    /// Define the <see cref="NeuralNetwork"/> activation function
+    /// </summary>
+    public enum ActivationFunction
+    {
+        /// <summary>
+        /// A non activation function
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// A sigmoid-type function
+        /// </summary>
+        Sigmoid = 1,
+
+        /// <summary>
+        /// A linear-type function
+        /// </summary>
+        Linear = 2,
+
+        /// <summary>
+        /// A gaussian-type function
+        /// </summary>
+        Gaussian = 3
+    }
+
     /// <summary>
     /// Implement a neural network
     /// </summary>
     public class NeuralNetwork
     {
-        private int[] layers;    
-        private float[][] neurons;    
-        private float[][] biases;    
-        private float[][][] weights;    
-        private int[] activations;
+        private double[][] inputs;
+        private double[][] outputs;
 
-        private float fitness;
+        private int layerCount;
+        private int epochCount;
+
+        private BasicNetwork nn;
+
+        private IMLDataSet trainingSet;
+        private IMLTrain train;
 
         /// <summary>
-        /// The fitness
+        /// The network inputs
         /// </summary>
-        public float Fitness { get => fitness; set => fitness = value; }
+        public double[][] Inputs { get => inputs; set => inputs = value; }
+
+        /// <summary>
+        /// The network outptus
+        /// </summary>
+        public double[][] Outputs { get => outputs; }
+
+        /// <summary>
+        /// The network train error
+        /// </summary>
+        public double Error => train.Error;
+
+        /// <summary>
+        /// The network layer counter
+        /// </summary>
+        public int LayerCount => layerCount;
+
+        /// <summary>
+        /// The network epoch counter
+        /// </summary>
+        public int EpochCount => epochCount;
 
         /// <summary>
         /// Create a new instance of <see cref="NeuralNetwork"/>
         /// </summary>
-        /// <param name="layers">The network layer</param>
-        public NeuralNetwork(int[] layers)
+        public NeuralNetwork()
         {
-            this.layers = layers;
-
-            InitializeNeurons();
-            InitializeBiases();
-            InitializeWeights();
+            nn = new BasicNetwork();
         }
 
         /// <summary>
-        /// Create an empty storage for the neurons in the network
-        /// </summary>
-        private void InitializeNeurons()
-        {
-            List<float[]> neuronsList = new List<float[]>();
-            for (int i = 0; i < layers.Length; i++)
-                neuronsList.Add(new float[layers[i]]);
-
-            neurons = neuronsList.ToArray();
-        }
-
-        /// <summary>
-        /// Initialize and populate the biases array being held within the network
-        /// </summary>
-        private void InitializeBiases()
-        {
-            List<float[]> biasList = new List<float[]>();
-            float[] bias;
-
-            for (int i = 0; i < layers.Length; i++)
-            {
-                bias = new float[layers[i]];
-                for (int j = 0; j < layers[i]; j++)
-                    bias[j] = GenerateRandomNumber();
-
-                biasList.Add(bias);
-            }
-
-            biases = biasList.ToArray();
-        }
-
-        /// <summary>
-        /// Initialize a random array for the weights being held in the network
-        /// </summary>
-        private void InitializeWeights()
-        {
-            List<float[][]> weightsList = new List<float[][]>();
-            List<float[]> layerWeightsList;
-            float[] neuronWeights;
-            int neuronsInPreviousLayer;
-
-            for (int i = 1; i < layers.Length; i++)
-            {
-                layerWeightsList = new List<float[]>();
-                neuronsInPreviousLayer = layers[i - 1];
-
-                for (int j = 0; j < neurons[i].Length; j++)
-                {
-                    neuronWeights = new float[neuronsInPreviousLayer];
-                    for (int k = 0; k < neuronsInPreviousLayer; k++)
-                        neuronWeights[k] = GenerateRandomNumber();
-
-                    layerWeightsList.Add(neuronWeights);
-                }
-
-                weightsList.Add(layerWeightsList.ToArray());
-            }
-
-            weights = weightsList.ToArray();
-        }
-
-        /// <summary>
-        /// Generate a random number between -0.5 and +0.5
-        /// </summary>
-        /// <returns>The random number generated</returns>
-        private float GenerateRandomNumber()
-        {
-            Random rnd = new Random(Guid.NewGuid().GetHashCode());
-
-            int min = -500, max = 501;
-            float value = rnd.Next(min, max) / 1000f;
-
-            return value;
-        }
-
-        /// <summary>
-        /// Generate a random number between 0 and <paramref name="max"/>
-        /// </summary>
-        /// <param name="max">The maximum value</param>
-        /// <returns>The random number generated</returns>
-        private float GenerateRandomNumber(int max)
-        {
-            Random rnd = new Random(Guid.NewGuid().GetHashCode());
-            float value = (float)(max * rnd.NextDouble());
-
-            return value;
-        }
-
-        /// <summary>
-        /// Generate  a random number between -<paramref name="limit"/> and
-        /// <paramref name="limit"/>
-        /// </summary>
-        /// <param name="limit">The range limit</param>
-        /// <returns>The random number generated</returns>
-        private float GenerateRandomNumber(float limit)
-        {
-            Random rnd = new Random(Guid.NewGuid().GetHashCode());
-            int sign = rnd.Next(-1, 1);
-
-            float value = (float)(sign * limit * rnd.NextDouble());
-            return value;
-        }
-
-        /// <summary>
-        /// Activation function
-        /// </summary>
-        /// <param name="value">The activation function input</param>
-        /// <returns>The activation function output</returns>
-        public float Activate(float value) => (float)Math.Tanh(value);
-
-        /// <summary>
-        /// Feed forward the inputs
+        /// Create a new instance of <see cref="NeuralNetwork"/>
         /// </summary>
         /// <param name="inputs">The network inputs</param>
-        /// <returns>The network outputs</returns>
-        public float[] FeedForward(float[] inputs)
+        /// <param name="outputs">The network outputs</param>
+        public NeuralNetwork(double[][] inputs, double[][] outputs) : this()
         {
-            for (int i = 0; i < inputs.Length; i++)
-                neurons[0][i] = inputs[i];
+            this.inputs = inputs;
+            this.outputs = outputs;
 
-            for (int i = 1; i < layers.Length; i++)
+            layerCount = 0;
+            epochCount = 0;
+
+            trainingSet = new BasicMLDataSet(inputs, outputs);
+        }
+
+        /// <summary>
+        /// Update the network data set
+        /// </summary>
+        public void UpdateDataSet()
+            => trainingSet = new BasicMLDataSet(inputs, outputs);
+
+        /// <summary>
+        /// Add a layer to the network
+        /// </summary>
+        /// <param name="neuronCounter">The layer neuron counter</param>
+        /// <param name="hasBias">The bias option</param>
+        /// <param name="activationFunction">The <see cref="ActivationFunction"/></param>
+        public void AddLayer(int neuronCounter, bool hasBias, ActivationFunction activationFunction = ActivationFunction.Sigmoid)
+        {
+            IActivationFunction af = null; // ActivationFunction.None
+            switch (activationFunction)
             {
-                int layer = i - 1;
-                for (int j = 0; j < neurons[i].Length; j++)
-                {
-                    float value = 0f;
-                    for (int k = 0; k < neurons[i - 1].Length; k++)
-                        value += weights[i - 1][j][k] * neurons[i - 1][k];
+                case ActivationFunction.Sigmoid:
+                    af = new ActivationSigmoid();
+                    break;
 
-                    neurons[i][j] = Activate(value + biases[i][j]);
-                }
+                case ActivationFunction.Linear:
+                    af = new ActivationLinear();
+                    break;
+
+                case ActivationFunction.Gaussian:
+                    af = new ActivationGaussian();
+                    break;
             }
 
-            return neurons[neurons.Length - 1];
+            nn.AddLayer(new BasicLayer(af, hasBias, neuronCounter));
+            layerCount++;
         }
 
         /// <summary>
-        /// Compare <see cref="NeuralNetwork"/> performances
+        /// Update the network structure
         /// </summary>
-        /// <param name="nn">The other <see cref="NeuralNetwork"/></param>
-        /// <returns>The comparison output</returns>
-        public int CompareTo(NeuralNetwork nn)
+        /// <remarks>This method should be called after <see cref="AddLayer(int, bool, ActivationFunction)"/>
+        /// and only if the network has at least two <see cref="LayerCount"/> and before calling
+        /// <see cref="Train(double)"/>!</remarks>
+        public void UpdateNetwork()
         {
-            int output = 0;
-
-            if (nn == null || fitness > nn.fitness)
-                output = 1;
-            else 
-                if (fitness < nn.fitness)
-                    output = -1;
-
-            return output;
-        }
-
-        /// <summary>
-        /// Load the weights and biases from a file int the network
-        /// </summary>
-        /// <param name="path">The file path</param>
-        public void LoadFromPath(string path)
-        {
-            TextReader tr = new StreamReader(path);
-            int numberOfLines = (int)new FileInfo(path).Length;
-            string[] listLines = new string[numberOfLines];
-            int index = 1;
-
-            for (int i = 1; i < numberOfLines; i++)
-                listLines[i] = tr.ReadLine();
-
-            tr.Close();
-
-            if (new FileInfo(path).Length > 0)
+            if (layerCount >= 2)
             {
-                for (int i = 0; i < biases.Length; i++)
-                {
-                    for (int j = 0; j < biases[i].Length; j++)
-                    {
-                        biases[i][j] = float.Parse(listLines[index]);
-                        index++;
-                    }
-                }
-                for (int i = 0; i < weights.Length; i++)
-                {
-                    for (int j = 0; j < weights[i].Length; j++)
-                    {
-                        for (int k = 0; k < weights[i][j].Length; k++)
-                        {
-                            weights[i][j][k] = float.Parse(listLines[index]);
-                            index++;
-                        }
-                    }
-                }
+                nn.Structure.FinalizeStructure();
+                nn.Reset();
             }
         }
 
         /// <summary>
-        /// Simple mutation function for any genetic implementation
+        /// Train the network
         /// </summary>
-        /// <param name="chance">The chance</param>
-        /// <param name="value">The value</param>
-        public void Mutate(int chance, float value)
+        /// <param name="errorThreshold">The error threshold to achieve</param>
+        public void Train(double errorThreshold = 0.01)
         {
-            for (int i = 0; i < biases.Length; i++)
-                for (int j = 0; j < biases[i].Length; j++)
-                    biases[i][j] = (GenerateRandomNumber(chance) <= 5) ? biases[i][j] += GenerateRandomNumber(value) : biases[i][j];
+            train = new ResilientPropagation(nn, trainingSet);
 
-            for (int i = 0; i < weights.Length; i++)
-                for (int j = 0; j < weights[i].Length; j++)
-                    for (int k = 0; k < weights[i][j].Length; k++)
-                        weights[i][j][k] = (GenerateRandomNumber(chance) <= 5) ? weights[i][j][k] += GenerateRandomNumber(value): weights[i][j][k];
+            epochCount = 1;
+            do
+            {
+                train.Iteration();
+                epochCount++;
+            } while (train.Error > errorThreshold);
+        }
+
+        /// <summary>
+        /// Test the network
+        /// </summary>
+        /// <returns>A <see cref="List{T}"/> containing, in order, the array with the inputs, the outputs and the ideal results</returns>
+        public List<(double[] Inputs, double[] Outputs, double[] Ideals)> Test()
+        {
+            List<(double[] Inputs, double[] Outputs, double[] Ideals)> result = new List<(double[] Inputs, double[] Outputs, double[] Ideals)>();
+            (double[] Inputs, double[] Outputs, double[] Ideals) item;
+
+            foreach (IMLDataPair pair in trainingSet)
+            {
+                IMLData output = nn.Compute(pair.Input);
+
+                item.Inputs = new double[pair.Input.Count];
+                for (int i = 0; i < pair.Input.Count; i++)
+                    item.Inputs[i] = pair.Input[i];
+
+                item.Outputs = new double[output.Count];
+                for (int i = 0; i < output.Count; i++)
+                    item.Outputs[i] = output[i];
+
+                item.Ideals = new double[pair.Ideal.Count];
+                for (int i = 0; i < pair.Ideal.Count; i++)
+                    item.Ideals[i] = pair.Ideal[i];
+
+                result.Add(item);
+            }
+
+            return result;
         }
     }
 }
