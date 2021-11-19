d4824d4f989de8e4a89a9a0fb88b8b6df69f7388
Simone De Grandis <simodg95@gmail.com>
2021-11-19 17:12:41 +0100

    Add a class to filter data and its relative test project

---
+++ Signal.Processing.Tests/FilterTestClass.cs
@@ -0,0 +1,94 @@
+﻿using Hardware;
+using NUnit.Framework;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Signal.Processing.Tests
+{
+    [TestFixture]
+    public class FilterTestClass
+    {
+        private Filter simpleFilter;
+        private Filter filter;
+
+        private AnalogInput input;
+
+        private double fs = 1000; //sampling rate
+        private double fw = 5; //signal frequency
+        private double fn = 50; //noise frequency
+        private double n = 5; //number of periods to show
+        private double A = 1; //signal amplitude
+        private double N = 0.1; //noise amplitude
+        private int size; //sample size
+
+        private double[] t;
+        private double[] y;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            size = (int)(n * fs / fw);
+
+            simpleFilter = new Filter(8, 12, 1000);
+
+            input = new AnalogInput("AiInput", "V", "0.000");
+            filter = new Filter(8, 12, 1000, input);
+
+            t = Enumerable.Range(1, size).Select(p => p * 1 / fs).ToArray();
+            y = t.Select(p => (A * Math.Sin(2 * Math.PI * fw * p)) + (N * Math.Sin(2 * Math.PI * fn * p))).ToArray();
+
+            SaveArray(y, "signal.csv");
+        }
+
+        [Test]
+        public void FilterSamples()
+        {
+            double[] filtered = simpleFilter.FilterSamples(y);
+            SaveArray(filtered, "simple_log.csv");
+        }
+
+        [Test]
+        public void FilterIterative()
+        {
+            double[] filtered = new double[y.Length];
+            int i = 0;
+            bool updated = false;
+
+            filter.Output.ValueChanged += (object _, Core.ValueChangedEventArgs __) =>
+            {
+                updated = false;
+                filtered[i] = filter.Output.Value;
+                updated = true;
+            };
+
+            for (; i < y.Length; i++)
+            {
+                input.Value = y[i];
+
+                do { } while (!updated);
+            }
+
+            SaveArray(filtered, "channel_log.csv");
+        }
+
+        /// <summary>
+        /// Save an array to a text file
+        /// </summary>
+        /// <param name="a">The array to save</param>
+        /// <param name="name">The file name (with extension)</param>
+        private void SaveArray(double[] a, string name)
+        {
+            string str = "";
+            a.ToList().ForEach(x => str += $"{x.ToString("F16", CultureInfo.InvariantCulture)}{Environment.NewLine}");
+
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"filter_test//{name}");
+            string folder = Path.GetDirectoryName(path);
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            File.WriteAllText(path, str);
+        }
+    }
+}
---
+++ Signal.Processing/Filter.cs
@@ -0,0 +1,92 @@
+﻿using Hardware;
+using MathNet.Filtering;
+
+namespace Signal.Processing
+{
+    /// <summary>
+    /// Implement a class with filtering functionalitiesB
+    /// </summary>
+    public class Filter
+    {
+        private OnlineFilter filter;
+
+        private AnalogOutput output;
+
+        /// <summary>
+        /// The <see cref="Filter"/> output as an <see cref="AnalogOutput"/>
+        /// </summary>
+        /// <remarks>
+        /// This property will only be valorized if the instance of <see cref="Filter"/> has been created with
+        /// <see cref="Filter(double, double, Channel{double})"/> or <see cref="Filter(double, double, double, Channel{double})"/> <br/><br/>
+        /// The effects of this method are equivalent to the ones of using <see cref="FilterSamples(double[])"/>,
+        /// but the filtering action is not done offline in this case!
+        /// </remarks>
+        public AnalogOutput Output => output;
+
+        /// <summary>
+        /// Create a new instance of low-pass <see cref="Filter"/>
+        /// </summary>
+        /// <param name="cutoffFrequence">The cutoff frequency (in Hertz)</param>
+        /// <param name="sampleRate">The sample rate (in samples per second)</param>
+        public Filter(double cutoffFrequence, double sampleRate)
+        {
+            filter = OnlineFilter.CreateLowpass(ImpulseResponse.Finite, sampleRate, cutoffFrequence);
+        }
+
+        /// <summary>
+        /// Create a new instance of a low-pass filter <see cref="Filter"/> that processes each sample acquired
+        /// </summary>
+        /// <param name="cutoffFrequence">The cutoff frequency (in Hertz)</param>
+        /// <param name="sampleRate">The sample rate (in samples per second)</param>
+        /// <param name="input">The <see cref="Channel{T}"/> containing the samples to filter</param>
+        public Filter(double cutoffFrequence, double sampleRate, Channel<double> input) : this(cutoffFrequence, sampleRate)
+        {
+            output = new AnalogOutput($"{input.Code}.Filtered", input.MeasureUnit, input.Format);
+
+            input.ValueChanged += (object _, Core.ValueChangedEventArgs __) =>
+                output.Value = filter.ProcessSample(input.Value);
+        }
+
+        /// <summary>
+        /// Create a new instance of a band-pass <see cref="Filter"/>
+        /// </summary>
+        /// <param name="lowpassFrequency">The lowpass frequency (in Hertz)</param>
+        /// <param name="highpassFrequency">The highpass frequency (in Hertz)</param>
+        /// <param name="sampleRate">The sample rate (in samples per second)</param>
+        public Filter(double lowpassFrequency, double highpassFrequency, double sampleRate)
+        {
+            filter = OnlineFilter.CreateBandpass(ImpulseResponse.Finite, sampleRate, lowpassFrequency, highpassFrequency);
+        }
+
+        /// <summary>
+        /// Create a new instance of a band-pass <see cref="Filter"/> that processes each sample acquired
+        /// </summary>
+        /// <param name="lowpassFrequency">The lowpass frequency (in Hertz)</param>
+        /// <param name="highpassFrequency">The highpass frequency (in Hertz)</param>
+        /// <param name="sampleRate">The sample rate (in samples per second)</param>
+        /// <param name="input">The <see cref="Channel{T}"/> containing the samples to filter</param>
+        public Filter(double lowpassFrequency, double highpassFrequency, double sampleRate, Channel<double> input)
+            : this(lowpassFrequency, highpassFrequency, sampleRate)
+        {
+            output = new AnalogOutput($"{input.Code}.Filtered", input.MeasureUnit, input.Format);
+
+            input.ValueChanged += (object _, Core.ValueChangedEventArgs __) =>
+                output.Value = filter.ProcessSample(input.Value);
+        }
+
+        /// <summary>
+        /// Filter the data acquired
+        /// </summary>
+        /// <remarks>
+        /// The effects of this method are equivalent to the ones of using <see cref="Output"/>,
+        /// but the filtering action is not done online in this case!
+        /// </remarks>
+        /// <param name="samples">The samples to filter</param>
+        /// <returns>The filtered samples</returns>
+        public double[] FilterSamples(double[] samples)
+        {
+            double[] filteredSamples = filter.ProcessSamples(samples);
+            return filteredSamples;
+        }
+    }
+}
