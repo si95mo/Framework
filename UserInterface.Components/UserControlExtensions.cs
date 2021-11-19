f98a1b9b01aac4a313254c6137906a16510444bd
Simone De Grandis <simodg95@gmail.com>
2021-11-19 10:10:04 +0100

    Add an extension class for the user controls

---
+++ UserInterface.Components/UserControlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UserInterface.Controls
+{
+    public static class UserControlExtensions
+    {
+        /// <summary>
+        /// Perform an update on the <see cref="UserControl"/> from a different
+        /// thread than the one in which <paramref name="source"/> has been created
+        /// </summary>
+        /// <param name="source">The source control to update</param>
+        /// <param name="action">The update <see cref="Action"/></param>
+        public static void Update(this UserControl source, Action action)
+            => source.Invoke(new MethodInvoker(() => action()));
+    }
+}
