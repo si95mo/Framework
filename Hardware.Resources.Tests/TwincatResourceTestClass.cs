a7ef51b3c26d0c8a992fd9427fa533f523f3d0ba
Simone De Grandis <simodg95@gmail.com>
2021-11-22 14:24:07 +0100

    Add a twincat resource to hardware

---
+++ Hardware.Resources.Tests/TwincatResourceTestClass.cs
@@ -0,0 +1,34 @@
+﻿using dsian.TwinCAT.Ads.Server.Mock;
+using FluentAssertions;
+using Hardware.Twincat;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Hardware.Resources.Tests
+{
+    [TestFixture]
+    public class TwincatResourceTestClass
+    {
+        private string amsNetAddress = "LocalAddress";
+        private int port = 801;
+        private TwincatResource resource;
+        Mock server;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            server = new Mock((ushort)port, amsNetAddress);
+
+            resource = new TwincatResource("TwincatResource", amsNetAddress, port);
+            await resource.Start();
+
+            resource.Status.Value.Should().Be(ResourceStatus.Executing);
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ Hardware.Twincat/TwincatDigitalOutput.cs
@@ -0,0 +1,22 @@
+﻿namespace Hardware.Twincat
+{
+    public class TwincatDigitalOutput : TwincatChannel<bool>, ITwincatChannel
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="TwincatDigitalOutput"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="variableName">The variable name in Twincat</param>
+        /// <param name="resource">The <see cref="IResource"/></param>
+        public TwincatDigitalOutput(string code, string variableName, IResource resource) 
+            : base(code, variableName, resource, measureUnit: "", format: "0")
+        {
+            ValueChanged += TwincatAnalogOutput_ValueChanged;
+        }
+
+        private async void TwincatAnalogOutput_ValueChanged(object sender, Core.ValueChangedEventArgs e)
+        {
+            await (resource as TwincatResource).Send(code);
+        }
+    }
+}
