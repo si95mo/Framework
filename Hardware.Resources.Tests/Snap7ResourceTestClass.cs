e32868c8118ae92e4f0c5ab15b5ce2151be2f573
Simone De Grandis <simodg95@gmail.com>
2021-12-16 11:03:20 +0100

    Add a test class for the Snap7Resource

---
+++ Hardware.Resources.Tests/Snap7ResourceTestClass.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Hardware.Snap7;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Hardware.Resources.Tests
+{
+    [TestFixture]
+    public class Snap7ResourceTestClass
+    {
+        private Snap7Resource resource;
+        private string ipAddress = "0.0.0.0";
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            resource = new Snap7Resource("Snap7Resource", ipAddress, 0, 0);
+
+            await resource.Start();
+            resource.Status.Value.Should().Be(ResourceStatus.Executing);
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine(resource);
+        }
+    }
+}
