6c052fb3a6ed08323aa024359f66113a6329f823
Simone De Grandis <simodg95@gmail.com>
2022-01-03 09:56:57 +0100

    Clean up the code

---
+++ UserInterface.Controls.Tests/SpectrumPlotterForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace UserInterface.Controls.Tests
+{
+    partial class SpectrumPlotterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chart = new LiveCharts.WinForms.CartesianChart();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Location = new System.Drawing.Point(13, 13);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(775, 425);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "cartesianChart1";
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            // 
+            // SpectrumPlotterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart);
+            this.Name = "SpectrumPlotterForm";
+            this.Text = "SpectrumPlotterForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart chart;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+    }
+}
