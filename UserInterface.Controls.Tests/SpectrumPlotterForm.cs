a4eb59fc88b17bd44df4a2063a78c67067fc4289
Simone De Grandis <simodg95@gmail.com>
2022-01-03 10:08:29 +0100

    Add a real time FFT test application (winform)

--- UserInterface.Controls.Tests/SpectrumPlotterForm.cs
+++ UserInterface.Controls.Tests/SpectrumPlotterForm.cs
@@ -1,8 +1,12 @@
 ﻿using Hardware;
 using LiveCharts;
 using LiveCharts.Wpf;
+using Mathematics;
+using Signal.Processing;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,9 +21,11 @@ namespace UserInterface.Controls.Tests
         {
             InitializeComponent();
 
-            series = new LineSeries();
-            series.Title = "Spectrum";
-            series.Values = new ChartValues<double>();
+            series = new LineSeries()
+            {
+                Title = "Spectrum",
+                Values = new ChartValues<double>()
+            };
 
             chart.Series.Clear();
             chart.Series.Add(series);
@@ -39,19 +45,20 @@ namespace UserInterface.Controls.Tests
                 for (int i = 0; i < 1000; i++)
                 {
                     samples[i] = Math.Sin(i);
-                    await Task.Delay(1);
+                    await Task.Delay(0);
                 }
 
-                    input.Value = samples;
+                input.Value = samples;
             }
         }
 
         private void Input_ValueChanged(object sender, Core.ValueChangedEventArgs e)
         {
-            series.Values.Clear();
+            double[] magnitudes = Mathematics.Mathematics.CalculateMagnitudes(Fourier.FFT(input.Value));
 
-            for (int i = 0; i < input.Value.Length; i++)
-                series.Values.Add(input.Value[i]);
+            series.Values.Clear();
+            for (int i = 0; i < magnitudes.Length; i++)
+                series.Values.Add(magnitudes[i]);
         }
     }
 }
