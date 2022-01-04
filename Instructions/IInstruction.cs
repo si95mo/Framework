79c9c9e1461acfc15f54f420982a88341ce4dfab
Simone De Grandis <simodg95@gmail.com>
2022-01-04 14:56:19 +0100

    Add a new instruction logic and the relative test project

--- Instructions/IInstruction.cs
+++ Instructions/IInstruction.cs
@@ -1,7 +1,8 @@
 ﻿using Core;
 using Core.DataStructures;
 using Core.Parameters;
-using Devices;
+using System;
+using System.Threading.Tasks;
 
 namespace Instructions
 {
@@ -9,21 +10,7 @@ namespace Instructions
     /// Describe a generic instruction
     /// </summary>
     public interface IInstruction : IProperty
-    {
-        /// <summary>
-        /// The <see cref="Bag{T}"/>
-        /// containing all the available <see cref="Method"/>
-        /// </summary>
-        Bag<Method> Methods
-        { get; }
-
-        /// <summary>
-        /// The <see cref="Bag{T}"/> of all the <see cref="IDevice"/>
-        /// with <see cref="IInstruction"/> related methods
-        /// </summary>
-        Bag<IDevice> Devices
-        { get; set; }
-
+    {       
         /// <summary>
         /// The <see cref="IInstruction"/> <see cref="Bag{T}"/>
         /// of input <see cref="IParameter"/>
@@ -39,8 +26,38 @@ namespace Instructions
         { get; }
 
         /// <summary>
+        /// The start time of the <see cref="IInstruction"/>
+        /// </summary>
+        DateTime StartTime
+        { get; }
+
+        /// <summary>
+        /// The stop time of the <see cref="IInstruction"/>
+        /// </summary>
+        DateTime StopTime
+        { get; }
+
+        /// <summary>
+        /// The <see cref="IInstruction"/> succeeded property
+        /// </summary>
+        BooleanParameter Succeeded
+        { get; }
+
+        /// <summary>
+        /// The <see cref="IInstruction"/> failed property
+        /// </summary>
+        BooleanParameter Failed
+        { get; }
+
+        /// <summary>
+        /// The <see cref="IInstruction"/> order (for parallelism)
+        /// </summary>
+        int Order
+        { get; }
+
+        /// <summary>
         /// Execute the <see cref="IInstruction"/>
         /// </summary>
-        void Invoke();
+        Task ExecuteInstruction();
     }
 }
