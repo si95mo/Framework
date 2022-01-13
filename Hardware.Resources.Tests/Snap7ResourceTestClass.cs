77f4c85856bb9b0db36efea3d2c739f5d72b473c
Simone De Grandis <simodg95@gmail.com>
2022-01-13 12:16:48 +0100

    Update Snap7Resource and relative test class

--- Hardware.Resources.Tests/Snap7ResourceTestClass.cs
+++ Hardware.Resources.Tests/Snap7ResourceTestClass.cs
@@ -3,6 +3,9 @@ using FluentAssertions;
 using Hardware.Snap7;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Hardware.Resources.Tests
@@ -11,26 +14,60 @@ namespace Hardware.Resources.Tests
     public class Snap7ResourceTestClass
     {
         private Snap7Resource resource;
-        private string ipAddress = "0.0.0.0";
+        private string ipAddress = "127.0.0.1";
+        private int dataBlock = 2;
 
         private Snap7AnalogInput analogInput;
+        private Snap7AnalogOutput analogOutput;
 
         [OneTimeSetUp]
         public async Task Setup()
         {
-            resource = new Snap7Resource("Snap7Resource", ipAddress, rack: 0, slot: 0, pollingInterval: 1000);
-            resource.AddDataBlock(1, 16);
+            resource = new Snap7Resource("Snap7Resource", ipAddress, rack: 0, slot: 0, pollingInterval: 100);
+            resource.AddDataBlock(dataBlock: 2, size: 256);
+
+            analogInput = new Snap7AnalogInput(
+                "Snap7AnalogInput",
+                memoryAddress: 0,
+                dataBlock: dataBlock,
+                resource,
+                RepresentationBytes.Two,
+                NumericRepresentation.Int16
+            );
+            analogOutput = new Snap7AnalogOutput(
+                "Snap7AnalogOutput",
+                memoryAddress: 0,
+                dataBlock: dataBlock,
+                resource,
+                RepresentationBytes.Two,
+                NumericRepresentation.Int16
+            );
 
             await resource.Start();
-            // resource.Status.Value.Should().Be(ResourceStatus.Executing);
-
-            analogInput = new Snap7AnalogInput("Snap7AnalogInput", memoryAddress: 0, dataBlock: 1, resource, RepresentationBytes.Two, NumericRepresentation.Int16);
+            resource.Status.Value.Should().Be(ResourceStatus.Executing);
         }
 
         [Test]
-        public void Test()
+        public async Task Test()
         {
-            analogInput.Value.Should().Be(default);
+            int counter = 0;
+            List<double> inputs = new List<double>();
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            do
+            {
+                analogOutput.Value = counter++;
+                await Task.Delay(1000);
+
+                inputs.Add(analogInput.Value);
+
+                Console.WriteLine($"{DateTime.Now:HH.mm.ss.fff} >> Out: {analogOutput}; In: {analogInput}");
+            } while (sw.Elapsed.TotalMilliseconds <= 10000); // 10 seconds
+
+            sw.Stop();
+
+            inputs.Distinct().Count().Should().BeInRange(inputs.Count - 2, inputs.Count);
         }
     }
 }
