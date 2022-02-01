8e023a52f32f32e4ae353214494172e0d5d026af
Simone De Grandis <simodg95@gmail.com>
2022-02-01 11:31:57 +0100

    Update test class (to verify read/write operation in multiple data blocks)

--- Hardware.Resources.Tests/Snap7ResourceTestClass.cs
+++ Hardware.Resources.Tests/Snap7ResourceTestClass.cs
@@ -15,32 +15,54 @@ namespace Hardware.Resources.Tests
     {
         private Snap7Resource resource;
         private string ipAddress = "127.0.0.1";
-        private int dataBlock = 2;
+        private int firstDataBlock = 2, secondDataBlock = 3;
 
-        private Snap7AnalogInput analogInput;
-        private Snap7AnalogOutput analogOutput;
+        private Snap7AnalogInput firstAnalogInput, secondAnalogInput;
+        private Snap7AnalogOutput firstAnalogOutput, secondAnalogOutput;
 
         [OneTimeSetUp]
         public async Task Setup()
         {
             resource = new Snap7Resource("Snap7Resource", ipAddress, rack: 0, slot: 0, pollingInterval: 100);
             resource.AddDataBlock(dataBlock: 2, size: 256);
+            resource.AddDataBlock(dataBlock: 3, size: 256);
 
-            analogInput = new Snap7AnalogInput(
-                "Snap7AnalogInput",
+            firstAnalogInput = new Snap7AnalogInput(
+                "FirstAnalogInput",
                 memoryAddress: 0,
-                dataBlock: dataBlock,
+                dataBlock: firstDataBlock,
                 resource,
                 RepresentationBytes.Two,
-                NumericRepresentation.Int16
+                NumericRepresentation.Int16,
+                format: "0"
+            ); 
+            secondAnalogInput = new Snap7AnalogInput(
+                 "SecondAnalogInput",
+                 memoryAddress: 0,
+                 dataBlock: secondDataBlock,
+                 resource,
+                 RepresentationBytes.Two,
+                 NumericRepresentation.Int16,
+                format: "0"
+             );
+
+            firstAnalogOutput = new Snap7AnalogOutput(
+                "FirstAnalogOutput",
+                memoryAddress: 0,
+                dataBlock: firstDataBlock,
+                resource,
+                RepresentationBytes.Two,
+                NumericRepresentation.Int16,
+                format: "0"
             );
-            analogOutput = new Snap7AnalogOutput(
-                "Snap7AnalogOutput",
+            secondAnalogOutput = new Snap7AnalogOutput(
+                "SecondAnalogOutput",
                 memoryAddress: 0,
-                dataBlock: dataBlock,
+                dataBlock: secondDataBlock,
                 resource,
                 RepresentationBytes.Two,
-                NumericRepresentation.Int16
+                NumericRepresentation.Int16,
+                format: "0"
             );
 
             await resource.Start();
@@ -51,23 +73,28 @@ namespace Hardware.Resources.Tests
         public async Task Test()
         {
             int counter = 0;
-            List<double> inputs = new List<double>();
+            List<double> firstInputs = new List<double>();
+            List<double> secondInputs = new List<double>();
 
             Stopwatch sw = Stopwatch.StartNew();
 
             do
             {
-                analogOutput.Value = counter++;
+                firstAnalogOutput.Value = counter++;
+                secondAnalogOutput.Value = 2 * counter;
                 await Task.Delay(1000);
 
-                inputs.Add(analogInput.Value);
+                firstInputs.Add(firstAnalogInput.Value);
+                secondInputs.Add(secondAnalogInput.Value);
 
-                Console.WriteLine($"{DateTime.Now:HH.mm.ss.fff} >> Out: {analogOutput}; In: {analogInput}");
+                Console.WriteLine($"{DateTime.Now:HH.mm.ss.fff} >> 1st out: {firstAnalogOutput}; 1st in: {firstAnalogInput}");
+                Console.WriteLine($"{DateTime.Now:HH.mm.ss.fff} >> 2nd out: {secondAnalogOutput}; 2nd in: {secondAnalogInput}");
             } while (sw.Elapsed.TotalMilliseconds <= 10000); // 10 seconds
 
             sw.Stop();
 
-            inputs.Distinct().Count().Should().BeInRange(inputs.Count - 2, inputs.Count);
+            firstInputs.Distinct().Count().Should().BeInRange(firstInputs.Count - 2, firstInputs.Count);
+            secondInputs.Distinct().Count().Should().BeInRange(secondInputs.Count - 2, secondInputs.Count);
         }
     }
 }
