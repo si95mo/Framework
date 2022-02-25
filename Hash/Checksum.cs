b3810273c80b731b6dc90610d8b29027d5775d63
Simone De Grandis <simodg95@gmail.com>
2022-02-25 08:35:47 +0100

    Add a project that implement hashing functionalities (CRC, MD5)

---
+++ Hash/Checksum.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace Hash
+{
+    /// <summary>
+    /// Class that calculate the CRC of a <see cref="string"/>
+    /// </summary>
+    public static class Checksum
+    {
+        /// <summary>
+        /// Calculate the checksum of a string by adding all the ASCII code
+        /// elements and converting the sum from base 10 to hex.
+        /// The checksum consists of the last 2 digits of the hex number.
+        /// </summary>
+        /// <param name="input">The string of which calculate the checksum</param>
+        /// <returns>The checksum, calculated as previously described</returns>
+        public static string CreateNew(string input)
+        {
+            string checksum = "";
+            int sum = 0;
+
+            for (int i = 0; i < input.Count(); i++)
+                sum += input[i];
+
+            checksum = sum.ToString("X");
+            checksum = "" + checksum.Substring(checksum.Length - 2);
+
+            return checksum;
+        }
+    }
+}
