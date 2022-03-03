16189a5c07677700a5cf649950bcdc629de64f07
Simone De Grandis <simodg95@gmail.com>
2022-03-03 09:47:43 +0100

    Add an hysteresis controller and its relative test project

---
+++ Control.Hysteresis.Tests/TestClass.cs
@@ -0,0 +1,37 @@
+﻿using Hardware;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace Control.Hysteresis.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        private double r = 1.0; // 1 Ohm
+        private double c = 1.0; // 1 F
+        private Stopwatch t;
+
+        HysteresisRegulator regulator;
+
+        private AnalogInput u;
+        private DigitalOutput actuator;
+
+        private double CalculateVoltage()
+        {
+            double vc = regulator.Output.Value * (1 - Math.Exp(-t.Elapsed.TotalSeconds / (r * c)));
+            return vc;
+        }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            t = Stopwatch.StartNew();
+
+            u = new AnalogInput("U", "V", "0.000");
+            regulator = new HysteresisRegulator(code: "Regulator", feedbackChannel: u, actuatorChannel: actuator, 10.5, 9.5, 10d, 10d);
+
+            regulator.Start();
+        }
+    }
+}
---
+++ Control.Hysteresis/HysteresisRegulator.cs
@@ -0,0 +1,90 @@
+﻿using Core.Parameters;
+using Hardware;
+using System;
+using System.Threading.Tasks;
+
+namespace Control.Hysteresis
+{
+    /// <summary>
+    /// Implement an hysteresis <see cref="Regulator"/>
+    /// </summary>
+    public class HysteresisRegulator : Regulator
+    {
+        public NumericParameter UpperLimit;
+        public NumericParameter LowerLimit;
+        public TimeSpanParameter CycleTime;
+
+        private Channel<bool> actuatorChannel;
+        private bool doRegulate;
+        private Task controlTask;
+
+        /// <summary>
+        /// Create a new instance of <see cref="HysteresisRegulator"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="feedbackChannel">The feedback <see cref="Channel{T}"/></param>
+        /// <param name="actuatorChannel">The actuator <see cref="Channel{T}"/></param>
+        /// <param name="upperLimit">The upper limit</param>
+        /// <param name="lowerLimit">The lower limit</param>
+        /// <param name="setpoint">The setpoint</param>
+        /// <param name="cycleTime">The cycle time (in milliseconds)</param>
+        public HysteresisRegulator(string code, Channel<double> feedbackChannel, Channel<bool> actuatorChannel, 
+            double upperLimit, double lowerLimit, double setpoint, double cycleTime) : base(code, feedbackChannel, setpoint)
+        {
+            UpperLimit = new NumericParameter(nameof(UpperLimit), upperLimit, measureUnit: feedbackChannel.MeasureUnit, format: feedbackChannel.Format);
+            LowerLimit = new NumericParameter(nameof(LowerLimit), lowerLimit, measureUnit: feedbackChannel.MeasureUnit, format: feedbackChannel.Format);
+            CycleTime = new TimeSpanParameter(nameof(CycleTime), TimeSpan.FromMilliseconds(cycleTime));
+
+            this.actuatorChannel = actuatorChannel;
+
+            doRegulate = false;
+            controlTask = null;
+        }
+
+        /// <summary>
+        /// Create the control <see cref="Task"/>
+        /// </summary>
+        /// <returns>The (async) <see cref="Task"/></returns>
+        private Task CreateControlTask() => new Task(async () =>
+            {
+                while (doRegulate)
+                {
+                    if (feedbackChannel.Value > UpperLimit.Value)
+                        actuatorChannel.Value = false;
+                    else
+                    {
+                        if (feedbackChannel.Value < LowerLimit.Value)
+                            actuatorChannel.Value = true;
+                    }
+
+                    await Task.Delay(CycleTime.Value);
+                }
+            }
+        );
+
+        public override void Start()
+        {
+            if (!doRegulate)
+                doRegulate = true;
+
+            if (controlTask == null)
+            {
+                controlTask = CreateControlTask();
+                controlTask.Start();
+            }
+            else
+            {
+                controlTask.Wait((int)CycleTime.Value.TotalMilliseconds);
+                controlTask.Dispose();
+
+                controlTask = CreateControlTask();
+                controlTask.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            doRegulate = false;
+        }
+    }
+}
