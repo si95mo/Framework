309e088bf300fc82f98b550da5198861be8ba067
Simone De Grandis <simodg95@gmail.com>
2022-03-08 16:38:15 +0100

    Add a task that provides Task extension methods

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Extensions
+{
+    /// <summary>
+    /// Provide <see cref="Task"/> (and <see cref="Task{TResult}"/>)-related extension methods
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Start a <see cref="Task"/> with a <paramref name="timeout"/>
+        /// </summary>
+        /// <param name="source">The <see cref="Task"/> to start</param>
+        /// <param name="timeout">The <paramref name="source"/> timeout (in milliseconds) to complete</param>
+        /// <returns>
+        /// The (async) <see cref="Task{TResult}"/> (<see langword="true"/> if the <see cref="Task"/> completed, 
+        /// <see langword="false"/> if timeout occurred)
+        /// </returns>
+        public static async Task<bool> StartWithTimeout(this Task source, int timeout)
+        {
+            bool completed = await Task.WhenAny(source, Task.Delay(timeout)) == source;
+            return completed;
+        }
+    }
+}
