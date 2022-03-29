918e6def2058f9a7ee47dd75a63edc99629f1b90
Simone De Grandis <simodg95@gmail.com>
2022-03-29 17:49:30 +0200

    Update TcpChannel (optional use of Send or SendAndReceive)

--- Hardware.Tcp/TcpChannel.cs
+++ Hardware.Tcp/TcpChannel.cs
@@ -12,6 +12,7 @@ namespace Hardware.Tcp
         private string response;
         private IResource resource;
         private int pollingInterval;
+        private bool useSendAndReceive;
         private Task pollingTask;
 
         /// <summary>
@@ -27,7 +28,11 @@ namespace Hardware.Tcp
                 {
                     response = "";
                     request = value;
-                    (Resource as TcpResource).SendAndReceive(this);
+
+                    if (useSendAndReceive)
+                        (Resource as TcpResource).SendAndReceive(this);
+                    else
+                        (Resource as TcpResource).Send(this);
                 }
             }
         }
@@ -60,7 +65,7 @@ namespace Hardware.Tcp
         /// </summary>
         /// <param name="code">The code</param>
         /// <param name="resource">The <see cref="IResource"/></param>
-        public TcpChannel(string code, IResource resource) : this(code, "", resource, 100, false)
+        public TcpChannel(string code, IResource resource) : this(code, "", resource, 100, false, true)
         { }
 
         /// <summary>
@@ -71,10 +76,15 @@ namespace Hardware.Tcp
         /// <param name="resource">The <see cref="IResource"/></param>
         /// <param name="pollingInterval">The polling interval (in milliseconds)</param>
         /// <param name="usePolling"><see langword="true"/> if polling must be used, <see langword="false"/> for async send/receive instead</param>
-        public TcpChannel(string code, string request, IResource resource, int pollingInterval = 100, bool usePolling = true) : base(code)
+        /// <param name="useSendAndReceive">Use a send/receive logic (the request is sent and then the response is awaited if <see langword="true"/>)</param>
+        public TcpChannel(string code, string request, IResource resource, int pollingInterval = 100, bool usePolling = true, bool useSendAndReceive = true) 
+            : base(code)
         {
             this.request = request;
             this.resource = resource;
+
+            this.pollingInterval = pollingInterval;
+            this.useSendAndReceive = useSendAndReceive;
 
             response = "";
             value = "";
@@ -83,7 +93,11 @@ namespace Hardware.Tcp
                 {
                     while (true)
                     {
-                        (resource as TcpResource).SendAndReceive(this);
+                        if (useSendAndReceive)
+                            (resource as TcpResource).SendAndReceive(this);
+                        else
+                            (resource as TcpResource).Send(this);
+
                         value = response;
 
                         await Task.Delay(pollingInterval);
