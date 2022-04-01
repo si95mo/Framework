c1170c2822b9a3f4b3a8c411a26ebdb1e5c539ac
Simone De Grandis <simodg95@gmail.com>
2022-04-01 11:16:53 +0200

    Add a waveform parameter

---
+++ Core.Parameters/WaveformParameter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Parameters
+{
+    public class WaveformParameter : Parameter<double[]>
+    {
+        /// <summary>
+        /// The <see cref="WaveformParameter"/> value;
+        /// </summary>
+        public override double[] Value => value;
+
+        /// <summary>
+        /// Create a new instance of <see cref="WaveformParameter"/>
+        /// </summary>
+        public WaveformParameter() : this(Guid.NewGuid().ToString())
+        { }
+
+        /// <summary>
+        /// Create a new instance of <see cref="WaveformParameter"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        public WaveformParameter(string code) : base(code)
+        { }
+
+        /// <summary>
+        /// Create a new instance of <see cref="WaveformParameter"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="format">The format</param>
+        /// <param name="measureUnit">The measure unit</param>
+        public WaveformParameter(string code, string measureUnit = "", string format = "") : this(code)
+        {
+            this.measureUnit = measureUnit;
+            this.format = format;
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="WaveformParameter"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="value">The initial value</param>
+        /// <param name="format">The format</param>
+        /// <param name="measureUnit">The measure unit</param>
+        public WaveformParameter(string code, double[] value, string measureUnit = "", string format = "")
+            : this(code, measureUnit: measureUnit, format: format)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// The <see cref="WaveformParameter"/> measure unit
+        /// </summary>
+        public new string MeasureUnit
+        {
+            get => measureUnit;
+            set => measureUnit = value;
+        }
+
+        /// <summary>
+        /// The <see cref="WaveformParameter"/> format
+        /// </summary>
+        public new string Format
+        {
+            get => format;
+            set => format = value;
+        }
+
+        /// <summary>
+        /// Return a description of the object
+        /// See also <see cref="object.ToString()"/>
+        /// </summary>
+        /// <returns>The description of the object</returns>
+        public override string ToString()
+        {
+            string description = double.NaN.ToString();
+
+            if (value != null)
+                description = $"{value.Average().ToString(format)}{measureUnit}";
+
+            return description;
+        }
+    }
+}
