8ec79f7bd2b666253a5df29558f57b40716d6761
Simone De Grandis <simodg95@gmail.com>
2022-04-04 17:49:38 +0200

    Update dashboard controls

---
+++ UserInterface.Dashboards/DashboardPanel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterface.Dashboards
+{
+    public partial class DashboardPanel : UserControl
+    {
+        private Graphics graphics;
+
+        protected Color LineColor { get; } = Color.FromArgb(0x28, 0x00, 0x00);
+        
+        public DashboardPanel()
+        {
+            InitializeComponent();
+
+            graphics = CreateGraphics();
+            DrawLines();
+        }
+
+        /// <summary>
+        /// Drag the lines grid
+        /// </summary>
+        public void DrawLines()
+        {
+            Pen pen = new Pen(LineColor, 0.001f);
+
+            for (int x = 0; x <= Size.Width; x += 16)
+            {
+                for (int y = 0; y < Size.Height; y += 16)
+                {
+                    graphics.DrawLine(pen, x, y, Size.Width, y);
+                    graphics.DrawLine(pen, x, y, x, Size.Height);
+                }
+            }
+        }
+
+        public void ClearLines()
+            => graphics.Clear(SystemColors.Control);
+    }
+}
---
+++ UserInterface.Dashboards/DraggableControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UserInterface.Dashboards
+{
+    /// <summary>
+    /// Implement a draggable control
+    /// </summary>
+    public partial class DraggableControl : UserControl
+    {
+        /// <summary>
+        /// Create a <see cref="DraggableControl"/>
+        /// </summary>
+        public DraggableControl()
+        {
+            InitializeComponent();
+        }
+
+        private void DraggableControl_Load(object sender, EventArgs e)
+            => this.Draggable(true);
+    }
+}
