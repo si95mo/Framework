e4a5e5f544d0883265fd4537f9033f411d140f70
Simone De Grandis <simodg95@gmail.com>
2022-04-13 12:06:40 +0200

    Add extension methods for tasks

--- Extensions/FrameworkExtensions.cs
+++ Extensions/FrameworkExtensions.cs
@@ -2,6 +2,7 @@
 using Core.Conditions;
 using Core.Parameters;
 using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -44,6 +45,12 @@ namespace Extensions
         public static TimeSpanParameter WrapToParameter(this TimeSpan source)
             => new TimeSpanParameter($"{nameof(source)}.AsParameter", source);
 
+        /// <summary>
+        /// Wait for an <see cref="ICondition"/> to be <see langword="true"/> without timeout
+        /// </summary>
+        /// <param name="_">The source</param>
+        /// <param name="condition">The <see cref="ICondition"/> to wait</param>
+        /// <returns>The (async) <see cref="Task"/></returns>
         public static async Task WaitFor(this object _, ICondition condition)
         {
             if (!condition.Value)
@@ -60,5 +67,87 @@ namespace Extensions
                 condition.ValueChanged -= eventHandler;
             }
         }
+
+        /// <summary>
+        /// Wait for an <see cref="ICondition"/> to be <see langword="true"/> with a timeout
+        /// </summary>
+        /// <param name="_">The source</param>
+        /// <param name="condition">The <see cref="ICondition"/> to wait</param>
+        /// <param name="timeout">The timeout (in milliseconds)</param>
+        /// <returns>
+        /// The (async) <see cref="Task{T}"/> (in which the result will be <see langword="true"/> if
+        /// the <paramref name="condition"/> became <see langword="true"/> before <paramref name="timeout"/> occurred,
+        /// <see langword="false"/> otherwise)
+        /// </returns>
+        public static async Task<bool> WaitFor(this object _, ICondition condition, int timeout)
+        {
+            bool result = true;
+
+            if(!condition.Value)
+            {
+                CancellationTokenSource tokenSource = new CancellationTokenSource();
+                EventHandler<ValueChangedEventArgs> eventHandler = (__, ___) =>
+                {
+                    if (condition.Value)
+                        tokenSource.Cancel();
+                };
+
+                condition.ValueChanged += eventHandler;
+
+                Stopwatch timer = Stopwatch.StartNew();
+                await Task.Delay(timeout, tokenSource.Token);
+                timer.Stop();
+
+                condition.ValueChanged -= eventHandler;
+
+                result = timer.Elapsed.TotalMilliseconds <= timeout;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Wait for a specific amount of time
+        /// </summary>
+        /// <param name="_">The source</param>
+        /// <param name="timeToWait">The time to wait (as <see cref="TimeSpan"/>)</param>
+        /// <returns>The (async) <see cref="Task"/></returns>
+        public static async Task WaitFor(this object _, TimeSpan timeToWait)
+            => await Task.Delay(timeToWait);
+
+        /// <summary>
+        /// Wait for a <see cref="Task"/> to complete without a timeout
+        /// </summary>
+        /// <param name="_">The source</param>
+        /// <param name="t">The <see cref="Task"/> to wait</param>
+        /// <returns>The (async) <see cref="Task"/></returns>
+        public static async Task WaitFor(this object _, Task t)
+            => await t;
+
+        /// <summary>
+        /// Wait for a <see cref="Task"/> to complete with timeout
+        /// </summary>
+        /// <param name="_">The source</param>
+        /// <param name="t">The <see cref="Task"/> to wait</param>
+        /// <param name="timeout">The timeout (in milliseconds)</param>
+        /// <returns>
+        /// The (async) <see cref="Task{TResult}"/> (<see langword="true"/> if the <see cref="Task"/> completed,
+        /// <see langword="false"/> if timeout occurred)
+        /// </returns>
+        public static async Task<bool> WaitFor(this object _, Task t, int timeout)
+            => await t.StartWithTimeout(timeout);
+
+        /// <summary>
+        /// Wait for a <see cref="Task"/> to complete with timeout
+        /// </summary>
+        /// <param name="_">The source</param>
+        /// <param name="t">The <see cref="Task"/> to wait</param>
+        /// <param name="timeout">The timeout (as <see cref="TimeSpan"/>)</param>
+        /// <returns>
+        /// The (async) <see cref="Task{TResult}"/> (<see langword="true"/> if the <see cref="Task"/> completed,
+        /// <see langword="false"/> if timeout occurred)
+        /// </returns>
+        public static async Task<bool> WaitFor(this object _, Task t, TimeSpan timeout)
+            => await _.WaitFor(t, timeout.Milliseconds);
     }
 }
--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Extensions
 {
@@ -21,5 +22,17 @@ namespace Extensions
             bool completed = await Task.WhenAny(source, Task.Delay(timeout)) == source;
             return completed;
         }
+
+        /// <summary>
+        /// Start a <see cref="Task"/> with a <paramref name="timeout"/>
+        /// </summary>
+        /// <param name="source">The <see cref="Task"/> to start</param>
+        /// <param name="timeout">The <paramref name="source"/> timeout (as <see cref="TimeSpan"/>) to complete</param>
+        /// <returns>
+        /// The (async) <see cref="Task{TResult}"/> (<see langword="true"/> if the <see cref="Task"/> completed,
+        /// <see langword="false"/> if timeout occurred)
+        /// </returns>
+        public static async Task<bool> StartWithTimeout(this Task source, TimeSpan timeout)
+            => await source.StartWithTimeout(timeout.Milliseconds);
     }
 }
