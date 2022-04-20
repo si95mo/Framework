f01fefc7962d5d3f479436c6465e6eb7f7548428
Simone De Grandis <simodg95@gmail.com>
2022-04-20 11:56:56 +0200

    Update doc comments

--- Diagnostic/Debugger.cs
+++ Diagnostic/Debugger.cs
@@ -7,7 +7,8 @@ namespace Diagnostic
     /// <summary>
     /// Implement a debugger that print to the <see cref="Debug"/> console. <br/>
     /// This class should be intended as a momentary logger to use during debug that prevents several
-    /// writing operation to file, but should be then disabled
+    /// writing operation to file, but should be then disabled (by setting <see cref="Enabled"/> <see langword="false"/>)
+    /// or completely removed from code
     /// </summary>
     public static class Debugger
     {
@@ -18,7 +19,8 @@ namespace Diagnostic
 
         /// <summary>
         /// Write a new line in the debug console and append
-        /// an <see cref="Environment.NewLine"/> at the end of it
+        /// an <see cref="Environment.NewLine"/> at the end of it if 
+        /// <see cref="Enabled"/> is <see langword="true"/>
         /// </summary>
         /// <param name="message">The message to write</param>
         public static void WriteLine(string message)
@@ -29,7 +31,8 @@ namespace Diagnostic
 
         /// <summary>
         /// Write a <see cref="StackTrace"/> to the debug console and append
-        /// an <see cref="Environment.NewLine"/> at the end of it
+        /// an <see cref="Environment.NewLine"/> at the end of it if 
+        /// <see cref="Enabled"/> is <see langword="true"/>
         /// </summary>
         /// <param name="stackTrace">The <see cref="StackTrace"/> to write</param>
         public static void WriteLine(StackTrace stackTrace)
@@ -37,7 +40,8 @@ namespace Diagnostic
 
         /// <summary>
         /// Write the <see cref="Exception.Message"/> and eventually the <see cref="Exception.StackTrace"/> to the debug console and
-        /// append an <see cref="Environment.NewLine"/> at the end of it
+        /// append an <see cref="Environment.NewLine"/> at the end of it if 
+        /// <see cref="Enabled"/> is <see langword="true"/>
         /// </summary>
         /// <param name="ex">The <see cref="Exception"/> to write</param>
         /// <param name="printStackTrace"><see langword="true"/> if <see cref="Exception.StackTrace"/> must be printed, <see langword="false"/> otherwise</param>
