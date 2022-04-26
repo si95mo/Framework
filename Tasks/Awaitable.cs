fd55dd5ee1404fa07e4a26a889352e356da6c953
Simone De Grandis <simodg95@gmail.com>
2022-04-26 15:36:48 +0200

    Add basics task-related class on a specific project

---
+++ Tasks/Awaitable.cs
@@ -0,0 +1,30 @@
+﻿using Core;
+using Core.Parameters;
+using System;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    /// <summary>
+    /// Implement generic aspect fo the <see cref="IAwaitable"/> interface
+    /// </summary>
+    public abstract class Awaitable : IAwaitable, IProperty
+    {
+        private string code;
+
+        public abstract EnumParameter<TaskStatus> Status { get; protected set; }
+        public abstract StringParameter Message { get; protected set; }
+
+        public string Code => code;
+        public object ValueAsObject { get => Status.Value; set => _; }
+
+        public Type Type => GetType();
+
+        protected Awaitable (string code)
+        {
+            this.code = code;
+        }
+
+        public abstract Task Start();
+    }
+}
---
+++ Tasks/IAwaitable.cs
@@ -0,0 +1,27 @@
+﻿using Core.Parameters;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    /// <summary>
+    /// Define a generic awaitable task
+    /// </summary>
+    public interface IAwaitable
+    {
+        /// <summary>
+        /// The <see cref="IAwaitable"/> <see cref="TaskStatus"/>
+        /// </summary>
+        EnumParameter<TaskStatus> Status { get; }
+
+        /// <summary>
+        /// The actua <see cref="IAwaitable"/> message (i.e. last executed operation)
+        /// </summary>
+        StringParameter Message { get; }
+
+        /// <summary>
+        /// Start the <see cref="IAwaitable"/> task
+        /// </summary>
+        /// <returns>The actual <see cref="Task"/> associated with the <see cref="IAwaitable"/></returns>
+        Task Start();
+    }
+}
