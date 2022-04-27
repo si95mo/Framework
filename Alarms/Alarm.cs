bf7888420b833d5fa2162841c9c2d4395ee842cf
Simone De Grandis <simodg95@gmail.com>
2022-04-27 10:52:26 +0200

    Add basic alarm logic (to be fully implemented)

---
+++ Alarms/Alarm.cs
@@ -0,0 +1,50 @@
+﻿using Core;
+using Core.Conditions;
+using System;
+
+namespace Alarms
+{
+    public class Alarm : IProperty, IAlarm
+    {
+        private string code;
+        private string source;
+        private string message;
+        private DateTime firingTime;
+
+        public string Code => code;
+
+        public object ValueAsObject { get => Code; set => _ = value; }
+
+        public Type Type => GetType();
+
+        public string Source => source;
+
+        public string Message => message;
+
+        public DateTime FiringTime => firingTime;
+
+        /// <summary>
+        /// Create a new instance of <see cref="Alarm"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="source">The source</param>
+        /// <param name="message">The message</param>
+        /// <param name="firingCondition">The <see cref="ICondition"/> that will cause the </param>
+        public Alarm(string code, string source, string message, ICondition firingCondition)
+        {
+            this.code = code;
+            this.source = source;
+            this.message = message;
+
+            firingCondition.ValueChanged += FiringCondition_ValueChanged;
+        }
+
+        private void FiringCondition_ValueChanged(object sender, ValueChangedEventArgs e)
+            => Fire();
+
+        public void Fire()
+        {
+            firingTime = DateTime.Now;
+        }
+    }
+}
---
+++ Alarms/IAlarm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Alarms
+{
+    /// <summary>
+    /// Define a basic alarm interface
+    /// </summary>
+    public interface IAlarm
+    {
+        /// <summary>
+        /// The source that caused the <see cref="IAlarm"/> to <see cref="Fire"/>
+        /// </summary>
+        string Source { get; }
+
+        /// <summary>
+        /// The <see cref="IAlarm"/> message
+        /// </summary>
+        string Message { get; }
+
+        /// <summary>
+        /// The <see cref="IAlarm"/> firing time (as <see cref="DateTime"/>
+        /// </summary>
+        DateTime FiringTime { get; }
+
+        /// <summary>
+        /// Fire the <see cref="IAlarm"/>
+        /// </summary>
+        void Fire();
+    }
+}
