c8b6e64c8df793f327a642f36d9c60467202b49b
Simone De Grandis <simodg95@gmail.com>
2022-05-16 11:31:24 +0200

    Add a channel control to UI components

---
+++ UserInterface.Components/ChannelControl.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace UserInterface.Controls
+{
+    partial class ChannelControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValue = new UserInterface.Controls.LabelControl();
+            this.lblCode = new UserInterface.Controls.LabelControl();
+            this.SuspendLayout();
+            // 
+            // lblValue
+            // 
+            this.lblValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F);
+            this.lblValue.Location = new System.Drawing.Point(327, 6);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(117, 20);
+            this.lblValue.TabIndex = 3;
+            this.lblValue.Text = "labelControl1";
+            // 
+            // lblCode
+            // 
+            this.lblCode.AutoSize = true;
+            this.lblCode.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F);
+            this.lblCode.Location = new System.Drawing.Point(90, 6);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(117, 20);
+            this.lblCode.TabIndex = 2;
+            this.lblCode.Text = "labelControl1";
+            // 
+            // ChannelControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.lblCode);
+            this.Name = "ChannelControl";
+            this.Size = new System.Drawing.Size(727, 32);
+            this.Load += new System.EventHandler(this.ChannelControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LabelControl lblValue;
+        private LabelControl lblCode;
+    }
+}
---
+++ UserInterface.Components/ChannelControl.cs
@@ -0,0 +1,59 @@
+﻿using Hardware;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace UserInterface.Controls
+{
+    public partial class ChannelControl : UserControl
+    {
+        private IChannel channel;
+        private Panel parent;
+
+        /// <summary>
+        /// Create a new instance of <see cref="ChannelControl"/>
+        /// </summary>
+        private ChannelControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="ChannelControl"/>
+        /// </summary>
+        /// <remarks>
+        /// It's necessary to pass the <paramref name="parent"/> container as a parameter
+        /// in order to correctly resize the <see cref="ChannelControl"/>!
+        /// </remarks>
+        /// <param name="channel">The <see cref="IChannel"/></param>
+        /// <param name="parent">The <see cref="ChannelControl"/> parent <see cref="Panel"/></param>
+        public ChannelControl(IChannel channel, Panel parent = null) : this()
+        {
+            this.channel = channel;
+            this.parent = parent;
+
+            lblCode.Text = channel.Code;
+            lblValue.Text = channel.ToString();
+        }
+
+        private void ChannelControl_Load(object sender, EventArgs e)
+        {
+            if (parent != null)
+            {
+                int width = parent.Size.Width - SystemInformation.VerticalScrollBarWidth - 4;
+                int heigth = Size.Height;
+                Size = new Size(width, heigth);
+            }
+
+            channel.ValueChanged += Channel_ValueChanged;
+        }
+
+        private void Channel_ValueChanged(object sender, Core.ValueChangedEventArgs e)
+        {
+            if (!InvokeRequired)
+                lblValue.Text = channel.ToString();
+            else
+                BeginInvoke(new Action(() => Channel_ValueChanged(sender, e)));
+        }
+    }
+}
