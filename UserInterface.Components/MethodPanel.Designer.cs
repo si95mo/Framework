e533ca91733aabf640b4ce15366c166460dcb3c2
Simone De Grandis <simodg95@gmail.com>
2022-05-18 17:09:24 +0200

    Update UI

---
+++ UserInterface.Components/MethodPanel.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Scheduling.Tests
+{
+    partial class MethodContainer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMethodNamePlaceholder = new System.Windows.Forms.Label();
+            this.lblMethodName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMethodNamePlaceholder
+            // 
+            this.lblMethodNamePlaceholder.AutoSize = true;
+            this.lblMethodNamePlaceholder.Location = new System.Drawing.Point(0, 24);
+            this.lblMethodNamePlaceholder.Name = "lblMethodNamePlaceholder";
+            this.lblMethodNamePlaceholder.Size = new System.Drawing.Size(75, 13);
+            this.lblMethodNamePlaceholder.TabIndex = 0;
+            this.lblMethodNamePlaceholder.Text = "Method name:";
+            // 
+            // lblMethodName
+            // 
+            this.lblMethodName.AutoSize = true;
+            this.lblMethodName.Location = new System.Drawing.Point(115, 24);
+            this.lblMethodName.Name = "lblMethodName";
+            this.lblMethodName.Size = new System.Drawing.Size(35, 13);
+            this.lblMethodName.TabIndex = 1;
+            this.lblMethodName.Text = "label2";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.Location = new System.Drawing.Point(261, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(36, 36);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // MethodContainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblMethodName);
+            this.Controls.Add(this.lblMethodNamePlaceholder);
+            this.Name = "MethodContainer";
+            this.Size = new System.Drawing.Size(300, 60);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMethodNamePlaceholder;
+        private System.Windows.Forms.Label lblMethodName;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
---
+++ UserInterface.Components/MethodPanel.cs
@@ -0,0 +1,148 @@
+﻿using Core.DataStructures;
+using Core.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace UserInterface.Controls
+{
+    /// <summary>
+    /// An user control to graphically visualize an instance of <see cref="Method"/>.
+    /// See also <see cref="UserControl"/>
+    /// </summary>
+    public partial class MethodControl : UserControl
+    {
+        private const int controlLocationOffset = 6;
+        private const int textBoxSize = 25;
+
+        private Size size;
+
+        private Method method;
+        private List<UserControl> values = new List<UserControl>();
+
+        /// <summary>
+        /// Create a new instance of the class <see cref="MethodControl"/>
+        /// </summary>
+        /// <param name="method">The <see cref="Method"/></param>
+        public MethodControl(Method method)
+        {
+            InitializeComponent();
+
+            size = Size;
+            btnAdd.Image = Properties.Resources.ImageAdd;
+
+            this.method = method;
+            ParameterList<MethodParameter> parameters = method.Parameters;
+
+            lblMethodName.Text = method.Name;
+
+            int parameterCount = (int)parameters?.Count;
+            if (parameterCount > 0)
+            {
+                size.Height += textBoxSize * parameterCount + 8;
+                Size = size;
+            }
+
+            int index = 1;
+            Label lblParameter;
+            if (parameters != null)
+            {
+                foreach (MethodParameter parameter in parameters)
+                {
+                    lblParameter = new Label();
+                    lblParameter.AutoSize = true;
+                    lblParameter.Text = parameter.Name;
+                    lblParameter.Size = lblMethodName.Size;
+                    lblParameter.Location = new Point(
+                        lblMethodName.Location.X + 10,
+                        lblMethodName.Location.Y + 10 + index++ * (lblMethodName.Size.Height + controlLocationOffset)
+                    );
+
+                    if (parameter.Type != ParameterType.Bool && parameter.Type != ParameterType.Enum)
+                    {
+                        TextControl txtCtrl = new TextControl
+                        {
+                            Location = new Point(lblMethodName.Location.X, lblParameter.Location.Y - 4)
+                        };
+
+                        // AddElementToCollection(lblParameter, txtCtrl);
+                    }
+                    else
+                    {
+                        if (parameter.Type == ParameterType.Bool)
+                        {
+                            DigitalControl diCtrl = new DigitalControl
+                            {
+                                Location = new Point(lblMethodName.Location.X, lblParameter.Location.Y - 4)
+                            };
+
+                            AddElementToCollection(lblParameter, diCtrl);
+                        }
+                        else
+                        {
+                            ComboControl cbxCtrl = new ComboControl
+                            {
+                                Location = new Point(lblMethodName.Location.X, lblParameter.Location.Y - 4)
+                            };
+
+                            BindingSource bs = new BindingSource
+                            {
+                                DataSource = Enum.GetValues(parameter.ExactType)
+                            };
+                            cbxCtrl.DataSource = bs;
+
+                            AddElementToCollection(lblParameter, cbxCtrl);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add an element to the collection
+        /// </summary>
+        /// <param name="lblParam">The <see cref="Label"/></param>
+        /// <param name="control">The <see cref="BaseControl"/></param>
+        private void AddElementToCollection(Label lblParam, UserControl control)
+        {
+            Controls.Add(lblParam);
+            Controls.Add(control);
+
+            values.Add(control);
+        }
+
+        /// <summary>
+        /// Add a <see cref="Method"/> to the <see cref="MethodScheduler"/>
+        /// <see cref="ActionQueue{T}"/>
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The <see cref="EventArgs"/></param>
+        private void BtnInvoke_Click(object sender, EventArgs e)
+        {
+            string[] parameters = new string[method.Parameters.Count];
+            for (int i = 0; i < parameters.Length; i++)
+                parameters[i] = method.Parameters[i].Value.ToString();
+
+            method.Invoke(parameters);
+        }
+
+        private void MethodControl_Paint(object sender, PaintEventArgs e)
+            => ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, Colors.BackgroundColor, ButtonBorderStyle.Solid);
+    }
+}
+
+// TODO: Improve UI for MethodControl
+/* Add new UserControls in order to have a better UI in respect to the type of the parameter, e.g.
+ * - A "MyUserComponent" that inherit from UserComponent and add a Value property
+ *   with the actual value
+ * All the other new UserComponents will inherit from "MyUserComponent".
+ * - A "DigitalButton" for boolean parameters
+ * - A "NumericTextBox" for integer and real numbers (the UserControl may have a format
+ *   to distinguish between the type of the number?)
+ * - A "StringComponent" for string parameters (inherit from TextBox and unify its style?)
+ *
+ * With all the new UserComponents (with a Value property with the actual value inserted by
+ * the user), txbValues should become a list of "MyUserComponent" and then:
+ * components?.ElementAt(i)?.Value
+ */
