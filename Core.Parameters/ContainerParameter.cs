6b9202d42127e50c56105074b46c6bc90c29419a
Simone De Grandis <simodg95@gmail.com>
2023-01-23 17:41:04 +0100

    Add container parameters

---
+++ Core.Parameters/ContainerParameter.cs
@@ -0,0 +1,49 @@
+﻿using Core.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Parameters
+{
+    /// <summary>
+    /// Define an abstract container parameter
+    /// </summary>
+    /// <typeparam name="T">The type of the contained objects</typeparam>
+    public abstract class ContainerParameter<T> : Parameter<T>, IContainerParameter where T: IParameter
+    {
+        /// <summary>
+        /// The <see cref="Bag{T}"/> of sub parameters
+        /// </summary>
+        public Bag<T> SubParameters { get; protected set; }
+
+        Bag<IParameter> IContainerParameter.SubParameters => (Bag<IParameter>)Convert.ChangeType(SubParameters, typeof(Bag<IParameter>));
+
+        /// <summary>
+        /// The <see cref="SubParameters"/> as a <see cref="List{T}"/>
+        /// </summary>
+        protected List<T> SubParametersAsList => (List<T>)Convert.ChangeType(SubParameters.ToList(), typeof(T));
+
+        /// <summary>
+        /// Initialize <see cref="ContainerParameter{T}"/> attributes
+        /// </summary>
+        /// <param name="code">The code</param>
+        protected ContainerParameter(string code) : base(code)
+        {
+            SubParameters = new Bag<T>();
+        }
+
+        /// <summary>
+        /// Add an item to the <see cref="IContainerParameter"/>
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+            => SubParameters.Add(item);
+
+        /// <summary>
+        /// Get an element from the <see cref="ContainerParameter{T}"/>
+        /// </summary>
+        /// <param name="code">The code of the element to get</param>
+        /// <returns>The retrieved element, or <see langword="default"/> if nothing was found</returns>
+        public IParameter Get(string code)
+            => SubParameters.Get(code);
+    }
+}
---
+++ Core.Parameters/IContainerParameter.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataStructures;
+
+namespace Core.Parameters
+{
+    /// <summary>
+    /// Define a container parameter interface
+    /// </summary>
+    public interface IContainerParameter : IParameter
+    {
+        /// <summary>
+        /// The <see cref="Bag{T}"/> of <see cref="IParameter"/>
+        /// </summary>
+        Bag<IParameter> SubParameters { get; }
+    }
+}
