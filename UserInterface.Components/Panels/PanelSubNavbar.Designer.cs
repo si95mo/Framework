fe581d4acedd44aa7064c8b909655c1e0822fc46
Simone De Grandis <simodg95@gmail.com>
2023-08-24 14:52:37 +0200

    Add date and time to PanelSubNavbar

--- UserInterface.Components/Panels/PanelSubNavbar.Designer.cs
+++ UserInterface.Components/Panels/PanelSubNavbar.Designer.cs
@@ -28,26 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTime = new UserInterface.Controls.LabelControl();
+            this.lblDate = new UserInterface.Controls.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.PbxLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PbxClose)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // LayoutPanel
+            // panel1
             // 
-            this.LayoutPanel.Size = new System.Drawing.Size(1920, 62);
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Location = new System.Drawing.Point(1733, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 62);
+            this.panel1.TabIndex = 4;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F);
+            this.lblTime.Location = new System.Drawing.Point(3, 5);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(181, 23);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Hour";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDate
+            // 
+            this.lblDate.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(3, 34);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(181, 23);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Date";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // PanelSubNavbar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.LayoutSize = new System.Drawing.Size(1920, 62);
+            this.Controls.Add(this.panel1);
             this.Name = "PanelSubNavbar";
+            this.Controls.SetChildIndex(this.PbxLogo, 0);
+            this.Controls.SetChildIndex(this.PbxClose, 0);
+            this.Controls.SetChildIndex(this.LayoutPanel, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
             ((System.ComponentModel.ISupportInitialize)(this.PbxLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PbxClose)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private LabelControl lblDate;
+        private LabelControl lblTime;
     }
 }
--- UserInterface.Components/Panels/PanelSubNavbar.cs
+++ UserInterface.Components/Panels/PanelSubNavbar.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace UserInterface.Controls.Panels
 {
@@ -20,6 +22,32 @@ namespace UserInterface.Controls.Panels
             PbxLogo.SendToBack();
 
             AutoScaleMode = AutoScaleMode.Inherit;
+
+            Load += PanelSubNavbar_Load;
+        }
+
+        private void PanelSubNavbar_Load(object sender, EventArgs e)
+        {
+            Task task = new Task(async () =>
+                {
+                    while (true)
+                    {
+                        if (!InvokeRequired)
+                            UpdateDateAndTime(DateTime.Now);
+                        else
+                            BeginInvoke(new Action(() => UpdateDateAndTime(DateTime.Now)));
+
+                        await Task.Delay(500); // Wait for 500ms, maximum time to not lose time information
+                    }
+                }
+            );
+            task.Start();
+        }
+
+        private void UpdateDateAndTime(DateTime dateTime)
+        {
+            lblTime.Text = dateTime.ToString("HH:mm:ss");
+            lblDate.Text = dateTime.ToString("yyyy/MM/dd");
         }
     }
 }
