e32dc0061ed1b9133401d82c372bff7450acde81
Simone De Grandis <simodg95@gmail.com>
2023-09-13 17:44:54 +0200

    Update ui

--- UserInterface.Components/Classes/Colors.cs
+++ UserInterface.Components/Classes/Colors.cs
@@ -9,74 +9,62 @@ namespace UserInterface.Controls
     public class Colors
     {
         /// <summary>
-        /// The background <see cref="Color"/>
-        /// defined as 0x8A, 0x9D, 0xC7
+        /// The background <see cref="Color"/> defined as 0xFA, 0xFA, 0xFA
         /// </summary>
-        public static readonly Color BackgroundColor = Color.FromArgb(0x8A, 0x9D, 0xC7);
+        public static readonly Color BackgroundColor = Color.FromArgb(0xFA, 0xFA, 0xFA);
 
         /// <summary>
-        /// The green <see cref="Color"/>
-        /// defined as 0xC7, 0xD1, 0x17
+        /// The green <see cref="Color"/> defined as 0xC7, 0xD1, 0x17
         /// </summary>
         public static readonly Color Green = Color.FromArgb(0xC7, 0xD1, 0x17);
 
         /// <summary>
-        /// The black <see cref="Color"/>
-        /// defined as 0x2F, 0x4F, 0x4F
+        /// The black <see cref="Color"/> defined as 0x2F, 0x4F, 0x4F
         /// </summary>
         public static readonly Color Black = Color.DarkSlateGray;
 
         /// <summary>
-        /// The text light <see cref="Color"/>.
-        /// See <see cref="Color.Transparent"/>
+        /// The text light <see cref="Color"/>. See <see cref="Color.Transparent"/>
         /// </summary>
         public static readonly Color Transparent = Color.Transparent;
 
         /// <summary>
-        /// The light text <see cref="Color"/>
-        /// defined as 0xFC, 0xEE, 0xDF
+        /// The light text <see cref="Color"/> defined as 0xFC, 0xEE, 0xDF
         /// </summary>
         public static readonly Color TextColorLight = Color.FromArgb(0xFC, 0xEE, 0xDF);
 
         /// <summary>
-        /// The dark text <see cref="Color"/>
-        /// defined as 0x2F, 0x4F, 0x4F
+        /// The dark text <see cref="Color"/> defined as 0x2F, 0x4F, 0x4F
         /// </summary>
         public static readonly Color TextColorDark = Color.DarkSlateGray;
 
         /// <summary>
-        /// The text <see cref="Color"/>
-        /// defined as 0x33, 0x33, 0x33
+        /// The text <see cref="Color"/> defined as 0x33, 0x33, 0x33
         /// </summary>
         public static readonly Color TextColor = Color.FromArgb(0x33, 0x33, 0x33);
 
         /// <summary>
-        /// The gray <see cref="Color"/>
-        /// defined as 0xC8, 0xC8, 0xC8
+        /// The gray <see cref="Color"/> defined as 0xC8, 0xC8, 0xC8
         /// </summary>
         public static readonly Color Grey = Color.FromArgb(0xC8, 0xC8, 0xC8);
 
         /// <summary>
-        /// The red <see cref="Color"/>
-        /// defined as 0xD2, 0x04, 0x2D
+        /// The red <see cref="Color"/> defined as 0xD2, 0x04, 0x2D
         /// </summary>
         public static readonly Color Red = Color.FromArgb(0xD2, 0x04, 0x2D);
 
         /// <summary>
-        /// The light blue <see cref="Color"/>
-        /// defined as 0xDE, 0xE7, 0xF2
+        /// The light blue <see cref="Color"/> defined as 0xDE, 0xE7, 0xF2
         /// </summary>
         public static readonly Color LightBlue = Color.FromArgb(0xDE, 0xE7, 0xF2);
 
         /// <summary>
-        /// The light yellow <see cref="Color"/>
-        /// defined as 0xFF, 0xFe, 0xED
+        /// The light yellow <see cref="Color"/> defined as 0xFF, 0xFe, 0xED
         /// </summary>
         public static readonly Color LightYellow = Color.FromArgb(0xFF, 0xFE, 0xED);
 
         /// <summary>
-        /// The yellow <see cref="Color"/>
-        /// defined as 0xFE, 0xD0, 0x00
+        /// The yellow <see cref="Color"/> defined as 0xFE, 0xD0, 0x00
         /// </summary>
         public static readonly Color Yellow = Color.FromArgb(0xFE, 0xD0, 0x00);
 
