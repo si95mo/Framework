f50090bb8632fbc48ea2f93a06e2ab47276bb454
Simone De Grandis <simodg95@gmail.com>
2023-11-02 14:46:11 +0100

    Add extension metbods for tasks

--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -9,17 +9,17 @@ namespace Extensions
     public static class TaskExtensions
     {
         /// <summary>
-        /// Start a <see cref="Task"/> with a <paramref name="timeout"/>
+        /// Start a <see cref="Task"/> with a <paramref name="timeoutInMilliseconds"/>
         /// </summary>
         /// <param name="source">The <see cref="Task"/> to start</param>
-        /// <param name="timeout">The <paramref name="source"/> timeout (in milliseconds) to complete</param>
+        /// <param name="timeoutInMilliseconds">The <paramref name="source"/> timeout (in milliseconds) to complete</param>
         /// <returns>
         /// The (async) <see cref="Task{TResult}"/> (<see langword="true"/> if the <see cref="Task"/> completed,
         /// <see langword="false"/> if timeout occurred)
         /// </returns>
-        public static async Task<bool> StartWithTimeout(this Task source, int timeout)
+        public static async Task<bool> StartWithTimeout(this Task source, int timeoutInMilliseconds)
         {
-            bool completed = await Task.WhenAny(source, Task.Delay(timeout)) == source;
+            bool completed = await Task.WhenAny(source, Task.Delay(timeoutInMilliseconds)) == source;
             return completed;
         }
 
@@ -34,5 +34,23 @@ namespace Extensions
         /// </returns>
         public static async Task<bool> StartWithTimeout(this Task source, TimeSpan timeout)
             => await source.StartWithTimeout(timeout.Milliseconds);
+
+        /// <summary>
+        /// Wait for a <see cref="Task"/> to end
+        /// </summary>
+        /// <param name="source">The source <see cref="Task"/></param>
+        /// <param name="timeoutInMilliseconds">The timeout in milliseconds</param>
+        /// <returns>The waiting <see cref="Task"/></returns>
+        public static async Task WaitFor(this Task source, int timeoutInMilliseconds)
+            => await source.StartWithTimeout(timeoutInMilliseconds);
+
+        /// <summary>
+        /// Wait for a <see cref="Task"/> to end
+        /// </summary>
+        /// <param name="source">The source <see cref="Task"/></param>
+        /// <param name="timeout">The timeout as a <see cref="TimeSpan"/></param>
+        /// <returns>The waiting <see cref="Task"/></returns>
+        public static async Task WaitFor(this Task source, TimeSpan timeout)
+            => await source.StartWithTimeout(timeout);
     }
 }
