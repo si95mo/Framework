a117e991cead01f4402add0bcd7b8f5e48fcd2e5
Simone De Grandis <simodg95@gmail.com>
2023-11-25 12:31:58 +0100

    Add a stream parameter

---
+++ Core.Parameters/IStreamParameter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Core.Parameters
+{
+    public interface IStreamParameter : IParameter<byte[]>
+    {
+        /// <summary>
+        /// The <see cref="IStreamParameter"/> <see cref="System.Text.Encoding"/>
+        /// </summary>
+        Encoding Encoding { get; }
+
+        /// <summary>
+        /// The encoded value
+        /// </summary>
+        string EncodedValue { get; set; }
+    }
+}
---
+++ Core.Parameters/StreamParameter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Core.Parameters
+{
+    public class StreamParameter : Parameter<byte[]>, IStreamParameter
+    {
+        public Encoding Encoding { get; protected set; }
+
+        public string EncodedValue
+        {
+            get => Encoding.GetString(Value);
+            set => Value = Encoding.GetBytes(value);
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="StreamParameter"/>
+        /// </summary>
+        /// <param name="code">The code</param>
+        /// <param name="encoding">The <see cref="Encoding"/></param>
+        public StreamParameter(string code, Encoding encoding) : base(code)
+        {
+            Encoding = encoding;
+        }
+    }
+}
