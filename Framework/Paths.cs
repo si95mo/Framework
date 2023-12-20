8142ad9d172b661b3c50c4bfbaa1afb58c67f4bb
Simone De Grandis <simodg95@gmail.com>
2023-12-20 17:37:09 +0100

    Add an utility class with all the default paths used by the framework

---
+++ Framework/Paths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Framework
+{
+    /// <summary>
+    /// Provides the <b>default</b> paths used by the framework
+    /// </summary>
+    public static class Paths
+    {
+        /// <summary>
+        /// The log folder
+        /// </summary>
+        public const string Logs = @"logs\";
+        
+        /// <summary>
+        /// The error folder
+        /// </summary>
+        public const string Errors = Logs + @"errors\";
+
+        /// <summary>
+        /// The configuration folder
+        /// </summary>
+        public const string Configuration = @"config";
+
+        /// <summary>
+        /// The application folder
+        /// </summary>
+        public static string Application => AppDomain.CurrentDomain.BaseDirectory;
+    }
+}
